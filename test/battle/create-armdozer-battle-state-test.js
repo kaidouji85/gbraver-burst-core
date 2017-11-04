c19b94d10ac959483fa25cf616eafbf23eeeb724
竹内佑介 <kaidouji85@gmail.com>
2017-11-04 16:54:40 +0900

    ユニットテストの基本を作った

---
+++ test/battle/create-armdozer-battle-state-test.js
@@ -0,0 +1,19 @@
+// @flow
+import test from 'ava';
+import {createArmDozerBattleState} from "../../src/battle/create-armdozer-battle-state";
+import type {ArmDozerBasicStatus} from "../../src/flow-type";
+import {ArmDozerIdList} from "../../src/flow-type";
+
+test('追加されたパラメータに正しい値がセットされている', t => {
+  const testData: ArmDozerBasicStatus = {
+    id: ArmDozerIdList.SHIN_BRAVER,
+    name: 'name',
+    maxHp: 3000,
+    maxBattery: 5,
+    power: 2000,
+    speed: 2000
+  };
+  const ret = createArmDozerBattleState(testData);
+  t.is(ret.hp, testData.maxHp);
+  t.is(ret.battery, testData.maxBattery);
+});
