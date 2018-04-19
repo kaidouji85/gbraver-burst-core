03aa84635133fbf7dae22fa364054542a5dcc9b9
竹内佑介 <kaidouji85@gmail.com>
2018-04-19 12:58:28 +0900

    ターン変更ロジックを別ファイルに切り出した

--- src/effect/turn-change/do-turn-change.js
+++ src/effect/turn-change/do-turn-change.js
@@ -2,24 +2,16 @@
 
 import type {GameState} from "../../game-state/game-state";
 import type {PhaseName} from "../../phase/phase-name";
-import type {PlayerId} from "../../player/player";
+import {getNextActivePlayer} from "./next-active-player";
 
 export function doTurnChange(lastState: GameState, phase: PhaseName): GameState {
   return {
     ...lastState,
     phase,
-    activePlayerId: getNextActivePlayer(lastState),
+    activePlayerId: getNextActivePlayer(
+      lastState.activePlayerId,
+      lastState.players.map(v => v.playerId)
+    ),
     effect: {name: 'TurnChange'}
   };
-}
-
-function getNextActivePlayer(lastState: GameState): PlayerId {
-  const notActivePlayer = lastState.players
-    .filter(v => v.playerId !== lastState.activePlayerId)
-    .map(v => v.playerId);
-  if (notActivePlayer.length === 1) {
-    return notActivePlayer[0];
-  }
-
-  return '';
 }
