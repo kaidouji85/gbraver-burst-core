9d205e4198f7d6722ddd5effe5c732912ee155ba
竹内佑介 <kaidouji85@gmail.com>
2018-05-10 21:03:30 +0900

    バッテリーコマンドチェック関数をシンプルにした

--- src/effect/input-command/enable-command.js
+++ src/effect/input-command/enable-command.js
@@ -1,12 +1,16 @@
 // @flow
 import type {PlayerState} from "../../game-state/player-state";
 import type {Command} from "../../command/command";
-import {getEnableBatteryCommand} from "./enable-battery-command";
+import {getEnableBattery} from "./enable-battery";
 import {getEnableBurstCommand} from "./enable-burst-command";
+import type {BatteryCommand} from "../../command/battery";
 
 export function getEnableCommand(player: PlayerState): Command[] {
+  const enableBattery: number[] = getEnableBattery(player.armdozer);
+  const batteryCommand: BatteryCommand[] = enableBattery.map(v => ({type: 'BATTERY_COMMAND', battery: v}));
+
   return [
-    ...getEnableBatteryCommand(player.armdozer),
+    ...batteryCommand,
     ...getEnableBurstCommand(player.armdozer)
   ];
 }
