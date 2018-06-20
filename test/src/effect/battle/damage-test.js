d339e7a041aa6ce20dfbc26043807114cf0cf8ca
竹内佑介 <kaidouji85@gmail.com>
2018-06-20 23:21:57 +0900

    攻撃ヒット判定のユニットテストを追加した

--- test/src/effect/battle/damage-test.js
+++ test/src/effect/battle/damage-test.js
@@ -7,32 +7,24 @@ import {EMPTY_ARMDOZER} from "../../../data/empty-armdozer";
 import type {BatteryCommand} from "../../../../src/command/battery";
 import type {PlayerState} from "../../../../src/game-state/player-state";
 
-test('攻撃バッテリ - 防御バッテリ = 1 なら ダメージ = 攻撃力', t => {
-  const attacker: PlayerState = {
-    playerId: 'player1',
-    armdozer: createArmdozerState({...EMPTY_ARMDOZER, power: 2000})
-  };
-  const attackerBattery: BatteryCommand = {type: 'BATTERY_COMMAND', battery: 3};
-  const defender = {
-    playerId: 'player2',
-    armdozer: createArmdozerState({...EMPTY_ARMDOZER})
-  };
-  const defenderBattery: BatteryCommand = {type: 'BATTERY_COMMAND', battery: 2};
+const ATTACKER: PlayerState = {
+  playerId: 'player1',
+  armdozer: createArmdozerState({...EMPTY_ARMDOZER, power: 2000})
+};
 
-  t.is(normalHitDamage(attacker, attackerBattery, defender, defenderBattery), 2000);
+const DEFENDER: PlayerState = {
+  playerId: 'player2',
+  armdozer: createArmdozerState({...EMPTY_ARMDOZER})
+};
+
+test('攻撃バッテリ - 防御バッテリ = 1 なら ダメージ = 攻撃力', t => {
+  const attackerBattery: BatteryCommand = {type: 'BATTERY_COMMAND', battery: 3};
+  const defenderBattery: BatteryCommand = {type: 'BATTERY_COMMAND', battery: 2};
+  t.is(normalHitDamage(ATTACKER, attackerBattery, DEFENDER, defenderBattery), 2000);
 });
 
 test('攻撃バッテリ - 防御バッテリ >= 2 なら ダメージ = 攻撃力 + 100 * (攻撃バッテリ - 防御バッテリ - 1)', t => {
-  const attacker: PlayerState = {
-    playerId: 'player1',
-    armdozer: createArmdozerState({...EMPTY_ARMDOZER, power: 2000})
-  };
   const attackerBattery: BatteryCommand = {type: 'BATTERY_COMMAND', battery: 5};
-  const defender = {
-    playerId: 'player2',
-    armdozer: createArmdozerState({...EMPTY_ARMDOZER})
-  };
   const defenderBattery: BatteryCommand = {type: 'BATTERY_COMMAND', battery: 2};
-
-  t.is(normalHitDamage(attacker, attackerBattery, defender, defenderBattery), 2200);
+  t.is(normalHitDamage(ATTACKER, attackerBattery, DEFENDER, defenderBattery), 2200);
 });
