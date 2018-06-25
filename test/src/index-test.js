4bb0ceb5dd87000a744c050694a0248ee8f39e9d
竹内佑介 <kaidouji85@gmail.com>
2018-06-25 12:40:42 +0900

    0.2.4パッケージを作成した

--- test/src/index-test.js
+++ test/src/index-test.js
@@ -1,52 +1,51 @@
 // @flow
 import test from 'ava';
 import {start} from "../../src/index";
-import {ArmDozerIdList, ArmDozers} from "../../src/master/armdozers";
 import type {Player} from "../../src/player/player";
 import type {GameState} from "../../src/game-state/game-state";
 import {progress} from "../../src/progress/index";
+import {EMPTY_ARMDOZER} from "../data/empty-armdozer";
+
+const PLAYER1: Player = {
+  playerId: 'player1',
+  armdozer: {
+    ...EMPTY_ARMDOZER,
+    speed: 2000
+  }
+};
+
+const PLAYER2: Player = {
+  playerId: 'player2',
+  armdozer: {
+    ...EMPTY_ARMDOZER,
+    speed: 2000
+  }
+};
 
 test('初期状態を正しく作ることができる', t => {
-  const player1: Player = {
-    playerId: 'player1',
-    armdozer: ArmDozers.find(v => v.id === ArmDozerIdList.SHIN_BRAVER) || ArmDozers[0]
-  };
-  const player2: Player = {
-    playerId: 'player2',
-    armdozer: ArmDozers.find(v => v.id === ArmDozerIdList.NEO_LANDOZER) || ArmDozers[0]
-  };
-  const result: GameState[] = start(player1, player2);
-  t.is(result.length > 0, true);
+  const result: GameState[] = start(PLAYER1, PLAYER2);
+  t.is(0 < result.length, true);
   t.is(result[0].effect.name, 'InputCommand');
-  t.pass();
 });
 
 test('startで作った初期状態からゲームを進めることができる', t => {
-  const player1: Player = {
-    playerId: 'player1',
-    armdozer: ArmDozers.find(v => v.id === ArmDozerIdList.SHIN_BRAVER) || ArmDozers[0]
-  };
-  const player2: Player = {
-    playerId: 'player2',
-    armdozer: ArmDozers.find(v => v.id === ArmDozerIdList.NEO_LANDOZER) || ArmDozers[0]
-  };
-  const initialState = start(player1, player2);
+  const initialState = start(PLAYER1, PLAYER2);
 
   const command1 = {
     playerId: 'player1',
     command: {
       type: 'BATTERY_COMMAND',
-      battery: 0
+      battery: 3
     }
   };
   const command2 = {
     playerId: 'player2',
     command: {
       type: 'BATTERY_COMMAND',
-      battery: 0
+      battery: 2
     }
   };
-  const update = progress(initialState[0], [command1, command2]);
-  t.is(update.length > 0, true);
-  t.pass();
+  const update = progress(initialState[initialState.length - 1], [command1, command2]);
+  t.is(0 < update.length, true, '状態更新は1レコード以上ある');
+  t.is(update[update.length - 1].effect.name, 'InputCommand', '最後の状態はコマンド入力である');
 });
