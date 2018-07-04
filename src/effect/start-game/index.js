c875f284b3d20f30890d766e5812fa24a705e18b
竹内佑介 <kaidouji85@gmail.com>
2018-07-04 12:56:33 +0900

    コメントを追加

--- src/effect/start-game/index.js
+++ src/effect/start-game/index.js
@@ -5,6 +5,13 @@ import type {GameState} from "../../game-state/game-state";
 import {createOpenPlayerState} from "../../game-state/player-state";
 import {getFirstTurnPlayer} from "./first-turn-payer";
 
+/**
+ * プレイヤー情報を受け取り、初期ゲームステートを生成する
+ *
+ * @param player1 プレイヤー1
+ * @param player2 プレイヤー2
+ * @return 初期ゲームステート
+ */
 export function startGame(player1: Player, player2: Player): GameState {
   const openPlayerStateList = [player1, player2].map(v => createOpenPlayerState(v));
 
