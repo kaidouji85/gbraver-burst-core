413e69f531cf6ce767bcb70beceb9e31b0ad3bc7
竹内佑介 <kaidouji85@gmail.com>
2018-07-18 12:59:30 +0900

    テストデータにcreateArmdozerStateを使うのをやめた

--- test/src/effect/battle/result/guard-test.js
+++ test/src/effect/battle/result/guard-test.js
@@ -2,13 +2,12 @@
 
 import test from 'ava';
 import type {PlayerState} from "../../../../../src/game-state/player-state";
-import {createArmdozerState} from "../../../../../src/game-state/armdozer-state";
-import {EMPTY_ARMDOZER} from "../../../../data/empty-armdozer";
+import {EMPTY_ARMDOZER_STATE} from "../../../../data/empty-armdozer";
 import {guard} from "../../../../../src/effect/battle/result/guard";
 
 const ATTACKER: PlayerState = {
   playerId: 'player1',
-  armdozer: createArmdozerState({...EMPTY_ARMDOZER, power: 2000})
+  armdozer: {...EMPTY_ARMDOZER_STATE, power: 2000}
 };
 
 test('防御なので、攻撃力の半分のダメージが与えられる', t => {
--- test/src/effect/battle/result/normal-hit-test.js
+++ test/src/effect/battle/result/normal-hit-test.js
@@ -2,19 +2,18 @@
 
 import test from 'ava';
 import type {PlayerState} from "../../../../../src/game-state/player-state";
-import {createArmdozerState} from "../../../../../src/game-state/armdozer-state";
-import {EMPTY_ARMDOZER} from "../../../../data/empty-armdozer";
+import {EMPTY_ARMDOZER_STATE} from "../../../../data/empty-armdozer";
 import type {BatteryCommand} from "../../../../../src/command/battery";
 import {normalHit} from "../../../../../src/effect/battle/result/normal-hit";
 
 const ATTACKER: PlayerState = {
   playerId: 'player1',
-  armdozer: createArmdozerState({...EMPTY_ARMDOZER, power: 2000})
+  armdozer: {...EMPTY_ARMDOZER_STATE, power: 2000}
 };
 
 const DEFENDER: PlayerState = {
   playerId: 'player2',
-  armdozer: createArmdozerState({...EMPTY_ARMDOZER})
+  armdozer: {...EMPTY_ARMDOZER_STATE}
 };
 
 test('通常ヒットなので、ダメージ計算式通りのダメージが与えられる', t => {
