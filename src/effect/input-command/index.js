03403b0ee811333e007ee361c57f7565a6964b67
y.takeuchi <takeuchi.y@opst.co.jp>
2019-06-13 16:31:07 +0900

    バーストフェイズ後のインプットコマンドフェイズを追加した

--- src/effect/input-command/index.js
+++ src/effect/input-command/index.js
@@ -3,6 +3,8 @@
 import type {GameState} from "../../game-state/game-state";
 import {getEnableBatteryCommand} from "./enable-battery-command";
 import {getEnableBurstCommand} from "./enable-burst-command";
+import type {PlayerCommand} from "../../command/player-command";
+import {selectableCommandAfterBurst} from "./selectable-command-after-burst";
 
 /**
  * コマンド入力フェイズのステートを生成する
@@ -22,6 +24,30 @@ export function inputCommand(lastState: GameState): GameState {
           ...getEnableBurstCommand(v.armdozer)
         ]
       }))
+    }
+  };
+}
+
+/**
+ * バーストフェイズ後のコマンド入力フェイズのステートを生成する
+ *
+ * @param lastState 更新前の状態
+ * @param commands バーストフェイズ発生時に各プレイヤーが選択したコマンド
+ * @returns コマンド入力状態
+ */
+export function inputCommandAfterBurst(lastState: GameState, commands: PlayerCommand[]): GameState {
+  return {
+    ...lastState,
+    effect: {
+      name: 'InputCommand',
+      players: lastState.players.map(player => {
+        const playerCommand = commands.find(v => v.playerId === player.playerId);
+        const command = playerCommand ? selectableCommandAfterBurst(player, playerCommand.command) : [];
+        return {
+          playerId: player.playerId,
+          command: command
+        }
+      })
     }
   };
 }
