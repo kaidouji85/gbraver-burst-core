dcb213d8c920052eaf184492b87c2e58b3208ef2
y.takeuchi <takeuchi.y@opst.co.jp>
2019-06-28 13:35:29 +0900

    戦闘効果ユニットテストのフォルダ構成を変更した

--- test/src/effect/battle/result/feint-test.js
+++ test/src/effect/battle/result/feint-test.js
@@ -7,11 +7,17 @@ import {feint} from "../../../../../src/effect/battle/result/feint";
 test('フェイントで防御側がバッテリーを使った場合、防御側が動いたとみなす', t => {
   const defenderBattery: BatteryCommand = {type: 'BATTERY_COMMAND', battery: 2};
   const result = feint(defenderBattery);
-  t.true(result.isDefenderMoved);
+  t.deepEqual(result, {
+    name: 'Feint',
+    isDefenderMoved: true
+  });
 });
 
 test('フェイントで防御側がバッテリーを使った場合、防御側が動いていないとみなす', t => {
   const defenderBattery: BatteryCommand = {type: 'BATTERY_COMMAND', battery: 0};
   const result = feint(defenderBattery);
-  t.false(result.isDefenderMoved);
+  t.deepEqual(result, {
+    name: 'Feint',
+    isDefenderMoved: false
+  });
 });
--- test/src/effect/battle/result/guard-test.js
+++ test/src/effect/battle/result/guard-test.js
@@ -7,36 +7,7 @@ import {guard} from "../../../../../src/effect/battle/result/guard";
 import {EMPTY_PLAYER_STATE} from "../../../../data/player";
 import type {BatteryCommand} from "../../../../../src/command/battery";
 
-test('攻撃力の半分のダメージが与えられる', t => {
-  const attacker: PlayerState = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: 'attacker',
-    armdozer: {
-      ...EMPTY_ARMDOZER_STATE,
-      power: 2000,
-    }
-  };
-  const attackerBattery: BatteryCommand = {
-    type: 'BATTERY_COMMAND',
-    battery: 3
-  };
-  const defender: PlayerState = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: 'defender',
-  };
-  const defenderBattery: BatteryCommand = {
-    type: 'BATTERY_COMMAND',
-    battery: 3
-  };
-
-  const result = guard(attacker, attackerBattery, defender, defenderBattery);
-  t.deepEqual(result, {
-    name: 'Guard',
-    damage: 1000
-  });
-});
-
-test('ガードでも攻撃力補正は有効である', t => {
+test('通常ヒットの半分のダメージを受ける', t => {
   const attacker: PlayerState = {
     ...EMPTY_PLAYER_STATE,
     playerId: 'attacker',
