a7decf417f29965aab49e8e9aa7d3775017c9b9c
y.takeuchi <takeuchi.y@opst.co.jp>
2019-06-28 11:42:48 +0900

    ガードのダメージが、通常ヒット計算式の半分になるようにした

--- src/effect/battle/effect/guard.js
+++ src/effect/battle/effect/guard.js
@@ -1,6 +1,8 @@
 // @flow
 
 import type {PlayerState} from "../../../game-state/player-state";
+import type {BatteryCommand} from "../../../command/battery";
+import {normalHitDamage} from "../damage/damage";
 
 /** ガード */
 export type Guard = {
@@ -12,11 +14,14 @@ export type Guard = {
  * 防御の戦闘結果を生成する
  *
  * @param attacker 攻撃側プレイヤー
+ * @param attackerCommand 攻撃側バッテリー
+ * @param defender 防御側プレイヤー
+ * @param defenderCommand 防御側バッテリー
  * @return 防御の戦闘結果
  */
-export function guard(attacker: PlayerState): Guard {
+export function guard(attacker: PlayerState, attackerCommand: BatteryCommand, defender: PlayerState, defenderCommand: BatteryCommand): Guard {
   return {
     name: 'Guard',
-    damage: attacker.armdozer.power / 2
+    damage: normalHitDamage(attacker, attackerCommand, defender, defenderCommand) / 2
   };
 }
