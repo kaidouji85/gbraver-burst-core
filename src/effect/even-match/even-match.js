5f14ef1b58d2ba3fbb1c5c2a309da8bb87a9e03f
竹内佑介 <kaidouji85@gmail.com>
2019-09-04 18:57:24 +0900

    引き分けの効果を追加した

---
+++ src/effect/even-match/even-match.js
@@ -0,0 +1,6 @@
+// @flow
+
+/** 引き分け */
+export type EvenMatch = {
+  name: 'EvenMatch'
+};
--- src/effect/index.js
+++ src/effect/index.js
@@ -6,6 +6,7 @@ import type {TurnChange} from "./turn-change/turn-change";
 import type {StartGame} from "./start-game/start-game";
 import type {BurstEffect} from "./burst/burst-effect";
 import type {EndGame} from "./end-game/end-game";
+import type {EvenMatch} from "./even-match/even-match";
 
 /** ゲーム中に発生する効果をまとめたもの */
-export type Effect = StartGame | EndGame | InputCommand | Battle | TurnChange | BurstEffect;
+export type Effect = StartGame | EndGame | EvenMatch | InputCommand | Battle | TurnChange | BurstEffect;
