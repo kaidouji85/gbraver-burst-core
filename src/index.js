847ddf8c19e7f3ee1235bd474f531677f62c4e57
竹内佑介 <kaidouji85@gmail.com>
2019-09-06 12:38:08 +0900

    戦闘によるゲーム終了判定のユニットテストを追加した

--- src/index.js
+++ src/index.js
@@ -2,4 +2,3 @@
 export {ArmDozerIdList, ArmDozers} from './master/armdozers';
 export {start} from './start';
 export {progress} from './progress/index';
-export {gameEndJudging} from './game-end-judging';
