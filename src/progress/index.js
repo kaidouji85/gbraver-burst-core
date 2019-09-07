d0558f4d1cee5aa5daafab00154c730915775519
竹内佑介 <kaidouji85@gmail.com>
2019-09-07 18:34:30 +0900

    バーストフローを作り直した

--- src/progress/index.js
+++ src/progress/index.js
@@ -1,13 +1,11 @@
 // @flow
+import * as R from 'ramda';
 import type {GameState} from "../game-state/game-state";
 import type {PlayerCommand} from "../command/player-command";
 import {battle} from "../effect/battle";
 import {turnChange} from "../effect/turn-change";
 import {inputCommand, inputCommandAfterBurst} from "../effect/input-command";
-import type {ApplyEffect} from "./apply-effects";
-import {applyEffects} from "./apply-effects";
 import {burst} from "../effect/burst";
-import type {PlayerId} from "../player/player";
 import {isBurstFlow} from "./is-burst-flow";
 import {gameEnd} from "../effect/game-end";
 import {gameEndJudging} from "../game-end-judging";
@@ -21,35 +19,33 @@ import {gameEndJudging} from "../game-end-judging";
  */
 export function progress(lastState: GameState, commands: PlayerCommand[]): GameState[] {
   if (isBurstFlow(commands)) {
-    return applyEffects(lastState, burstFlow(lastState.activePlayerId, commands));
+    return burstFlow(lastState, commands);
   }
 
   return battleFlow(lastState, commands);
 }
 
-/**
- * バースト関連の効果適用関数
- *
- * @param activePlayerId アクティブプレイヤーのID
- * @param commands コマンド
- * @return 効果適用関数
- */
-function burstFlow(activePlayerId: PlayerId, commands: PlayerCommand[]): ApplyEffect[] {
-  const attackerCommand = commands.find(v => v.playerId === activePlayerId);
-  const defenderCommand = commands.find(v => v.playerId !== activePlayerId);
+function burstFlow(lastState: GameState, commands: PlayerCommand[]): GameState[] {
+  const attackerCommand = commands.find(v => v.playerId === lastState.activePlayerId);
+  const defenderCommand = commands.find(v => v.playerId !== lastState.activePlayerId);
   if (!attackerCommand || !defenderCommand) {
     return [];
   }
 
-  return [
-    state => attackerCommand.command.type === 'BURST_COMMAND'
-      ? burst(state, attackerCommand.playerId)
-      : null,
-    state => defenderCommand.command.type === 'BURST_COMMAND'
-      ? burst(state, defenderCommand.playerId)
-      : null,
-    state => inputCommandAfterBurst(state, commands)
-  ];
+  const getLastState = (history: GameState[]) => history[history.length - 1];
+  return R.pipe(
+    (history: GameState[]): GameState[] =>
+      attackerCommand.command.type === 'BURST_COMMAND'
+        ? [...history, burst(getLastState(history), attackerCommand.playerId)]
+        : history,
+    (history: GameState[]): GameState[] =>
+      defenderCommand.command.type === 'BURST_COMMAND'
+        ? [...history, burst(getLastState(history), defenderCommand.playerId)]
+        : history,
+    (history: GameState[]): GameState[] => {
+      return [...history, inputCommandAfterBurst(getLastState(history), commands)];
+    },
+  )([lastState]).slice(1);
 }
 
 function battleFlow(lastState: GameState, commands: PlayerCommand[]): GameState[] {
