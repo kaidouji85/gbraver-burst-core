1e576a3d5d277e5c5b84eb872b1e57d8c9342145
竹内佑介 <kaidouji85@gmail.com>
2020-04-02 22:10:09 +0900

    HistoryUpdateからhistory引数を削除した

--- src/game/progress/game-flow.js
+++ src/game/progress/game-flow.js
@@ -6,10 +6,9 @@ import type {GameState} from "../../state/game-state";
  * ステートヒストリー更新関数
  *
  * @param lastState 最新の状態
- * @param history ここまでの履歴
  * @return 更新内容
  */
-export type HistoryUpdate = (state: GameState, history: GameState[]) => GameState[];
+export type HistoryUpdate = (state: GameState) => GameState[];
 
 /**
  * ゲームフロー
@@ -21,6 +20,6 @@ export type HistoryUpdate = (state: GameState, history: GameState[]) => GameStat
 export function gameFlow(lastState: GameState, updateList: HistoryUpdate[]): GameState[] {
   return updateList.reduce((history: GameState[], update: HistoryUpdate) => {
     const state = history[history.length - 1];
-    return [...history,  ...update(state, history)];
+    return [...history,  ...update(state)];
   }, [lastState]).slice(1);
 }
