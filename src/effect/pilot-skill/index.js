ff8ff10e3f500ee1f12ece32620ae5a22edeee44
竹内佑介 <kaidouji85@gmail.com>
2020-08-01 21:56:39 +0900

    パイロットスキル使用済みの処理を追加した

--- src/effect/pilot-skill/index.js
+++ src/effect/pilot-skill/index.js
@@ -13,10 +13,53 @@ import {recoverBattery} from "./recover-battery";
  * @return 更新結果
  */
 export function pilotSkill(lastState: GameState, invokerId: PlayerId, skill: PilotSkill): GameState {
+  const donePilotSkill = pilotSkillEffect(lastState, invokerId, skill);
+  return disablePilotSkill(donePilotSkill, invokerId);
+}
+
+/**
+ * パイロットスキルを適用する
+ *
+ * @param lastState 最新の状態
+ * @param invokerId パイロットスキルを発動するプレイヤー
+ * @param skill パイロットスキルの内容
+ * @return 更新結果
+ */
+function pilotSkillEffect(lastState: GameState, invokerId: PlayerId, skill: PilotSkill): GameState {
   if (skill.type === 'RecoverBatterySkill') {
     const recoverBatterySkill: RecoverBatterySkill = skill;
     return recoverBattery(lastState, invokerId, recoverBatterySkill);
   }
 
   return lastState;
+}
+
+/**
+ * パイロットスキルを使用済みにする
+ *
+ * @param lastState 最新状態
+ * @param invokerId パイロットスキルを発動するプレイヤー
+ * @return 更新結果
+ */
+function disablePilotSkill(lastState: GameState, invokerId: PlayerId): GameState {
+  const invoker = lastState.players.find(v => v.playerId === invokerId);
+  if (!invoker) {
+    return lastState;
+  }
+
+  const updatedInvoker = {
+    ...invoker,
+    pilot: {
+      ...invoker.pilot,
+      enableSkill: false
+    }
+  };
+  const updatedPlayers = lastState.players.map(v => v.playerId === invokerId
+    ? updatedInvoker
+    : v
+  );
+  return {
+    ...lastState,
+    players: updatedPlayers
+  };
 }
