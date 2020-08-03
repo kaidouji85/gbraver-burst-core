08c46a21034e7ca30a9bafc0edf38b200d7fa5fc
竹内佑介 <kaidouji85@gmail.com>
2020-08-03 12:36:32 +0900

    gameStartInputCommandのユニットテストを別ファイルに分けた

--- test/src/effect/input-command/index-test.js
+++ test/src/effect/input-command/index-test.js
@@ -1,86 +1,12 @@
 // @flow
 
 import test from 'ava';
-import {gameStartInputCommand, inputCommand} from "../../../../src/effect/input-command";
+import {inputCommand} from "../../../../src/effect/input-command";
 import type {GameState} from "../../../../src/game/state/game-state";
 import {EMPTY_PLAYER_STATE} from "../../../data/player";
 import {EMPTY_ARMDOZER_STATE} from "../../../data/armdozer";
 import {EMPTY_GAME_STATE} from "../../../data/game-state";
 import type {PlayerCommand} from "../../../../src";
-
-test('ゲームスタート時 コマンド入力フェイズが正しく適用される', t => {
-  const lastState: GameState = {
-    ...EMPTY_GAME_STATE,
-    players: [
-      {
-        ...EMPTY_PLAYER_STATE,
-        playerId: 'player01',
-        armdozer: {
-          ...EMPTY_PLAYER_STATE.armdozer,
-          battery: 5,
-          maxBattery: 5,
-          enableBurst: true,
-        },
-        pilot: {
-          ...EMPTY_PLAYER_STATE.pilot,
-          enableSkill: true
-        }
-      },
-      {
-        ...EMPTY_PLAYER_STATE,
-        playerId: 'player02',
-        armdozer: {
-          ...EMPTY_PLAYER_STATE.armdozer,
-          battery: 5,
-          maxBattery: 5,
-          enableBurst: true,
-        },
-        pilot: {
-          ...EMPTY_PLAYER_STATE.pilot,
-          enableSkill: true
-        }
-      }
-    ]
-  };
-
-  const result = gameStartInputCommand(lastState);
-  t.deepEqual(result, {
-    ...lastState,
-    effect: {
-      name: 'InputCommand',
-      players: [
-        {
-          playerId: 'player01',
-          selectable: true,
-          command: [
-            {type: 'BATTERY_COMMAND', battery: 0},
-            {type: 'BATTERY_COMMAND', battery: 1},
-            {type: 'BATTERY_COMMAND', battery: 2},
-            {type: 'BATTERY_COMMAND', battery: 3},
-            {type: 'BATTERY_COMMAND', battery: 4},
-            {type: 'BATTERY_COMMAND', battery: 5},
-            {type: 'BURST_COMMAND'},
-            {type: 'PILOT_SKILL_COMMAND'}
-          ]
-        },
-        {
-          playerId: 'player02',
-          selectable: true,
-          command: [
-            {type: 'BATTERY_COMMAND', battery: 0},
-            {type: 'BATTERY_COMMAND', battery: 1},
-            {type: 'BATTERY_COMMAND', battery: 2},
-            {type: 'BATTERY_COMMAND', battery: 3},
-            {type: 'BATTERY_COMMAND', battery: 4},
-            {type: 'BATTERY_COMMAND', battery: 5},
-            {type: 'BURST_COMMAND'},
-            {type: 'PILOT_SKILL_COMMAND'}
-          ]
-        }
-      ]
-    }
-  });
-});
 
 test('戦闘後のコマンド入力フェイズが正しく適用される', t => {
   const lastState: GameState = {
