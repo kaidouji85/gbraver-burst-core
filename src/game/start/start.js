102db4150a774b3087ce1dab7d38597bf4713c4f
竹内佑介 <kaidouji85@gmail.com>
2020-09-14 13:41:09 +0900

    ゲームスタート適用関数のインタフェースを変更した

--- src/game/start/start.js
+++ src/game/start/start.js
@@ -3,6 +3,7 @@ import type {Player} from "../../player/player";
 import type {GameState} from "../state/game-state";
 import {startGame} from "../../effect/start-game";
 import {gameStartInputCommand} from "../../effect/input-command";
+import {upcastGameState} from "../state/game-state";
 
 /**
  * ゲームの初期状態を生成する
@@ -12,7 +13,8 @@ import {gameStartInputCommand} from "../../effect/input-command";
  * @return ゲーム初期状態
  */
 export function start(player1: Player, player2: Player): GameState[] {
-  const initialState = startGame(player1, player2);
+  const done = startGame(player1, player2);
+  const initialState = upcastGameState(done);
   return [
     initialState,
     gameStartInputCommand(initialState)
