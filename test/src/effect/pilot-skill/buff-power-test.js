937be440989e8459898b2474b0c0f1caeae746d6
竹内佑介 <kaidouji85@gmail.com>
2020-09-29 13:48:23 +0900

    パイロットスキル攻撃バフのユニットテストを追加した

---
+++ test/src/effect/pilot-skill/buff-power-test.js
@@ -0,0 +1,61 @@
+// @flow
+
+import test from 'ava';
+import type {GameState, PlayerState} from "../../../../src";
+import {EMPTY_PLAYER_STATE} from "../../../data/player";
+import {EMPTY_GAME_STATE} from "../../../data/game-state";
+import type {BuffPowerSkill} from "../../../../src/player/pilot";
+import {buffPower} from "../../../../src/effect/pilot-skill/buff-power";
+
+test('攻撃バフスキルが正しく処理できる', t => {
+  const invoker: PlayerState = {
+    ...EMPTY_PLAYER_STATE,
+    playerId: 'invoker',
+    armdozer: {
+      ...EMPTY_PLAYER_STATE.armdozer,
+      maxBattery: 5,
+      battery: 2,
+    }
+  };
+  const skill: BuffPowerSkill = {
+    type: 'BuffPowerSkill',
+    buffPower: 600,
+    duration: 2
+  };
+  const other: PlayerState = {
+    ...EMPTY_PLAYER_STATE,
+    playerId: 'other'
+  };
+  const state: GameState = {
+    ...EMPTY_GAME_STATE,
+    players: [other, invoker]
+  };
+
+  const result = buffPower(state, invoker.playerId, skill);
+  const expected = {
+    ...state,
+    players: [
+      other,
+      {
+        ...invoker,
+        armdozer: {
+          ...invoker.armdozer,
+          effects: [
+            ...invoker.armdozer.effects,
+            {
+              type: 'CorrectPower',
+              power: 600,
+              remainingTurn: 2,
+            }
+          ]
+        }
+      }
+    ],
+    effect: {
+      name: 'PilotSkillEffect',
+      invokerId: invoker.playerId,
+      skill: skill
+    }
+  };
+  t.deepEqual(result, expected);
+});
