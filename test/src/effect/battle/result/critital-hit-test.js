396524b29b0c8d3125185bc48bf7040b1367f81b
竹内佑介 <kaidouji85@gmail.com>
2020-11-30 09:11:40 +0900

    クリティカルヒットダメージ計算式ユニットテストに、攻撃補正、ダメージ減少の条件を追加した

--- test/src/effect/battle/result/critital-hit-test.js
+++ test/src/effect/battle/result/critital-hit-test.js
@@ -7,22 +7,32 @@ import type {BatteryCommand} from "../../../../../src/command/battery";
 import {criticalHit} from "../../../../../src/effect/battle/result/critical-hit";
 import {EMPTY_PLAYER_STATE} from "../../../../data/player";
 
-test('クリティカルヒットなので、ダメージ計算式の2倍のダメージが与えられる', t => {
+test('クリティカルヒットが正しく計算できる', t => {
   const attacker: PlayerState = {
     ...EMPTY_PLAYER_STATE,
     playerId: 'player1',
-    armdozer: {...EMPTY_ARMDOZER_STATE, power: 2000}
+    armdozer: {
+      ...EMPTY_ARMDOZER_STATE, power: 2000,
+      effects: [
+        {type: 'CorrectPower', power: 600, remainingTurn: 2}
+      ]
+    }
   };
   const defender: PlayerState = {
     ...EMPTY_PLAYER_STATE,
     playerId: 'player2',
-    armdozer: {...EMPTY_ARMDOZER_STATE}
+    armdozer: {
+      ...EMPTY_ARMDOZER_STATE,
+      effects: [
+        {type: "DamageDecrease", decrease: 600, remainingTurn: 2}
+      ]
+    }
   };
   const attackerBattery: BatteryCommand = {type: 'BATTERY_COMMAND', battery: 2};
   const defenderBattery: BatteryCommand = {type: 'BATTERY_COMMAND', battery: 0};
   const result = criticalHit(attacker, attackerBattery, defender, defenderBattery);
   t.deepEqual(result, {
     name: 'CriticalHit',
-    damage: 4200
+    damage: 4800
   });
 });
