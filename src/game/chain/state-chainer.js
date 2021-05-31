a576428b8bc59eb0c8ee6f22010fa176dfcfff82
Yuusuke <kaidouji85@gmail.com>
2021-05-31 10:51:09 +0000

    JSDocを追加した

--- src/game/chain/state-chainer.js
+++ src/game/chain/state-chainer.js
@@ -2,27 +2,79 @@
 
 import type {GameStateX, GameState} from '../../state/game-state';
 
+/** 
+ * ゲームステートチェイナー
+ * 
+ * @template X 最新のゲームステートの効果
+ */
 export interface StateChainer<X> {
+  /**
+   * ゲームステート履歴を取得する
+   * 
+   * @return 取得結果
+   */
   stateHistory(): GameState[];
+
+  /**
+   * 最新のゲームステートを取得する
+   * 
+   * @return 取得結果
+   */
   lastState(): GameStateX<X>;
+
+  /**
+   * ゲームステートをチェインする
+   * 
+   * @template Y チェイン後の最新ゲームステートの効果
+   * @param fn チェインする関数
+   * @return チェイン結果 
+   */
   chain<Y>(fn: (v: GameStateX<X>) => StateChainer<Y>): StateChainer<Y>;
 }
 
+/**
+ * シンプルなゲームステートチェイナー
+ * 
+ * @template X 最新のゲームステートの効果
+ */
 export class SimpleStateChainer<X> implements StateChainer<X> {
   _stateHistory: GameState[];
 
-  constructor(history: GameState[], state: GameStateX<X>) {
-    this._stateHistory = [...history, (state: any)];
+  /**
+   * コンストラクタ
+   * 
+   * @param history ゲームステート履歴 
+   * @param lastState 最新のゲームステート
+   */
+  constructor(history: GameState[], lastState: GameStateX<X>) {
+    this._stateHistory = [...history, (lastState: any)];
   }
 
+  /**
+   * 最新のゲームステートを取得する
+   * 
+   * @return 取得結果
+   */
   stateHistory(): GameState[] {
     return this._stateHistory;
   }
 
+  /**
+   * 最新のゲームステートを取得する
+   * 
+   * @return 取得結果
+   */
   lastState(): GameStateX<X> {
     return ((this.stateHistory[this.stateHistory.length - 1]): any);
   }
 
+  /**
+   * ゲームステートをチェインする
+   * 
+   * @template Y チェイン後の最新ゲームステートの効果
+   * @param fn チェインする関数
+   * @return チェイン結果 
+   */
   chain<Y>(fn: (v: GameStateX<X>) => StateChainer<Y>): StateChainer<Y> {
     const result = fn(this.lastState());
     const stateHistory = [...this.stateHistory(), ...result.stateHistory().slice(0, -1)];
