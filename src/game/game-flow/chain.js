9f076b92a7f50573266f5998e3a8273d81a0419a
竹内佑介 <kaidouji85@gmail.com>
2021-06-01 23:47:40 +0900

    chainのテストを追加した

--- src/game/game-flow/chain.js
+++ src/game/game-flow/chain.js
@@ -3,6 +3,12 @@
 import type {GameStateX} from '../../state/game-state';
 import {GameFlow} from './game-flow';
 
+/**
+ * ゲームフローに1個だけステートを追加する
+ *
+ * @param fn 更新関数
+ * @return 更新結果
+ */
 export function chain<X, Y>(fn: (v: GameStateX<X>) => GameStateX<Y>): ((v: GameFlow<X>) => GameFlow<Y>) {
   return (v: GameFlow<X>): GameFlow<Y> =>  {
     const lastState = fn(v.lastState);
---
+++ test/game/game-flow/chain-test.js
@@ -0,0 +1,17 @@
+// @flow
+
+import test from 'ava';
+import {chain} from '../../../src/game/game-flow/chain';
+import {GameFlow} from "../../../src/game/game-flow/game-flow";
+import {EMPTY_GAME_STATE} from "../../../src";
+
+test('ゲームステートを正しくチェインすることができる', t => {
+  const s1 = {...EMPTY_GAME_STATE, playerId: 'state01'};
+  const s2 = {...EMPTY_GAME_STATE, playerId: 'state02'};
+  const s3 = {...EMPTY_GAME_STATE, playerId: 'state03'};
+  const s4 = {...EMPTY_GAME_STATE, playerId: 'state04'};
+  const flowA = new GameFlow([s1, s2, s3], s3);
+  const result = flowA.to(chain(() => s4));
+  t.deepEqual(result.lastState, s4);
+  t.deepEqual(result.stateHistory, [s1, s2, s3, s4]);
+});
