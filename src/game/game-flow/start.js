512a03d309e84f3d8751d7c8861da764faa22587
竹内佑介 <kaidouji85@gmail.com>
2021-06-01 23:35:00 +0900

    ゲームフロー開始のユニットテストを追加した

--- src/game/game-flow/start.js
+++ src/game/game-flow/start.js
@@ -3,6 +3,13 @@
 import type {GameStateX} from '../../state/game-state';
 import {GameFlow} from './game-flow';
 
+/**
+ * ゲームフローを開始する
+ *
+ * @template X ゲームステートの効果
+ * @param state ゲームステート
+ * @return ゲームフロー
+ */
 export function start<X>(state: GameStateX<X>): GameFlow<X> {
   return new GameFlow([(state: any)], state);
 }
