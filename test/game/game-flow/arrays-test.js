140081d4690720b6936fe9e8cf761cdbfa252b00
竹内佑介 <kaidouji85@gmail.com>
2021-06-02 22:45:13 +0900

    配列ステート追加関数を追加した

---
+++ test/game/game-flow/arrays-test.js
@@ -0,0 +1,19 @@
+// @flow
+
+import test from 'ava';
+import {EMPTY_GAME_STATE} from "../../../src";
+import {GameFlow} from "../../../src/game/game-flow/game-flow";
+import {arrays} from "../../../src/game/game-flow/arrays";
+import {upcastGameState as up} from "../../../src/state/game-state";
+
+test('ゲームステート配列を正しく結合できる', t => {
+  const s1 = up({...EMPTY_GAME_STATE, activePlayerId: 's1'});
+  const s2 = up({...EMPTY_GAME_STATE, activePlayerId: 's1'});
+  const s3 = up({...EMPTY_GAME_STATE, activePlayerId: 's1'});
+  const s4 = up({...EMPTY_GAME_STATE, activePlayerId: 's1'});
+  const s5 = up({...EMPTY_GAME_STATE, activePlayerId: 's1'});
+  const flowA = new GameFlow([s1, s2], s2);
+  const result = flowA.to(arrays([s3, s4, s5]));
+  t.deepEqual(result.lastState, s5);
+  t.deepEqual(result.stateHistory, [s1, s2, s3, s4, s5]);
+});
