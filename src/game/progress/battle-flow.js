a103185094dfe5f84e37956664d2caac447717a3
竹内佑介 <kaidouji85@gmail.com>
2021-06-04 00:50:13 +0900

    不要なファイルを削除した

--- src/game/progress/battle-flow.js
+++ src/game/progress/battle-flow.js
@@ -1,6 +1,5 @@
 // @flow
 
-import {deprecated_gameFlow} from "../deprecated-flow/game-flow";
 import type {GameState} from '../../state/game-state';
 import {upcastGameState as up} from "../../state/game-state";
 import {batteryDeclaration} from "../../effect/battery-declaration";
@@ -8,9 +7,7 @@ import {battle} from "../../effect/battle";
 import {gameEndJudging} from "../end-judging";
 import {gameEnd} from "../../effect/game-end";
 import {canRightItself, rightItself} from "../../effect/right-itself";
-import {extractBatteryCommands} from "./extract-battery-commands";
-import type {PlayerCommand, PlayerCommandX} from "../command/player-command";
-import {deprecated_gameContinueFlow} from "./deprecated_game-continue-flow";
+import type {PlayerCommandX} from "../command/player-command";
 import type {BatteryCommand} from "../../command/battery";
 import {start} from "../game-flow/start";
 import {addHistory as add, chain} from "../game-flow/chain";
@@ -118,78 +115,4 @@ export function gameContinueFlow(state: GameState, attackerId: PlayerId, attacke
       : add(v, up(turnChange(v.lastState))))
     .to(chain(v => inputCommand(v, attackerId, attackerCommand, defenderId, defenderCommand)))
     .stateHistory.slice(1);
-}
-
-/**
- * @deprecated
- * 戦闘フロー
- *
- * @param lastState 最後の状態
- * @param commands コマンド
- * @return 更新されたゲームステート
- */
-export function deprecated_battleFlow(lastState: GameState, commands: [PlayerCommand, PlayerCommand]): GameState[] {
-  const batteries = extractBatteryCommands(lastState, commands);
-  if (!batteries) {
-    return [];
-  }
-
-  return deprecated_gameFlow(lastState, [
-    state => {
-      const doneBatteryDeclaration = batteryDeclaration(
-        state,
-        batteries.attacker.playerId,
-        batteries.attacker.command,
-        batteries.defender.playerId,
-        batteries.defender.command
-      );
-      if (!doneBatteryDeclaration) {
-        return [];
-      }
-
-      const doneBattle = battle(
-        up(doneBatteryDeclaration),
-        doneBatteryDeclaration.effect.attacker,
-        doneBatteryDeclaration.effect.attackerBattery,
-        batteries.defender.playerId,
-        doneBatteryDeclaration.effect.defenderBattery
-      );
-      if (!doneBattle) {
-        return [];
-      }
-
-      return [
-        up(doneBatteryDeclaration),
-        up(doneBattle),
-        ...deprecated_gameFlow(up(doneBattle), [
-          state => canReflectFlow(doneBattle.effect.result)
-            ? reflectFlow(state, doneBattle.effect.attacker)
-            : [],
-          state => {
-            if (!canRightItself(doneBattle.effect)) {
-              return [];
-            }
-
-            const done = rightItself(state, doneBattle.effect);
-            return done ? [up(done)] : [];
-          },
-          state => {
-            const endJudge = gameEndJudging(state);
-            if (endJudge.type === 'GameContinue') {
-              return deprecated_gameContinueFlow(
-                state,
-                batteries.attacker.playerId,
-                batteries.attacker.command,
-                batteries.defender.playerId,
-                batteries.defender.command
-              );
-            } else {
-              const done = gameEnd(state, endJudge);
-              return [up(done)];
-            }
-          }
-        ])
-      ];
-    }
-  ]);
 }
