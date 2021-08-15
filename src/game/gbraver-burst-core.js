eddd00a64bf2cd4c65034975dd66971df8b1118f
竹内佑介 <kaidouji85@gmail.com>
2021-08-15 17:20:35 +0900

    コアにダンプ、リストアを追加した

--- src/game/gbraver-burst-core.js
+++ src/game/gbraver-burst-core.js
@@ -8,42 +8,36 @@ import {isDuplicatePlayers} from "./validation/is-duplicate-players";
 import type {Player} from "../player/player";
 import {isAllPlayerEnteredCommand} from "./validation/is-all-player-entered-command";
 
-/** ゲームコア部分 */
-export class GbraverBurstCore {
-  _players: [Player, Player];
-  _stateHistory: GameState[];
+/** ゲームを再開するためのデータ */
+export type GbraverBurstCoreRestore = {
+  /** プレイヤー情報 */
+  players: [Player, Player];
+  /** ステートヒストリー */
+  stateHistory: GameState[];
+};
 
-  /**
-   * コンストラクタ
-   *
-   * @param players バトルに参加するプレイヤー
-   */
-  constructor(players: [Player, Player]) {
-    if (isDuplicatePlayers(players)) {
-      throw new Error('duplicate players');
-    }
-
-    this._players = players;
-    this._stateHistory = start(this._players);
-  }
-
+/** Gブレイバーバーストコア */
+export interface GbraverBurstCore {
   /**
    * バトルに参加している全プレイヤーを取得する
    *
    * @return 取得結果
    */
-  players(): [Player, Player] {
-    return this._players;
-  }
+  players(): [Player, Player];
 
   /**
    * ゲームステート履歴を取得する
    *
    * @return 取得結果
    */
-  stateHistory(): GameState[] {
-    return this._stateHistory;
-  }
+  stateHistory(): GameState[];
+
+  /**
+   * 現在の状態をダンプする
+   *
+   * @return ダンプしたデータ
+   */
+  dump(): GbraverBurstCoreRestore;
 
   /**
    * ゲームを進行させる
@@ -51,6 +45,66 @@ export class GbraverBurstCore {
    * @param commands コマンド
    * @return 更新されたゲーム状態
    */
+  progress(commands: [PlayerCommand, PlayerCommand]): GameState[];
+}
+
+/**
+ * Gブレイバーバーストを開始する
+ *
+ * @param players プレイヤー情報
+ * @return Gブレイバーバースト
+ */
+export function startGbraverBurst(players: [Player, Player]): GbraverBurstCore {
+  if (isDuplicatePlayers(players)) {
+    throw new Error('duplicate players');
+  }
+
+  const stateHistory = start(players);
+  return new GbraverBurstCoreImpl(players, stateHistory);
+}
+
+/**
+ * Gブレイバーバーストを再開する
+ *
+ * @param data 再開するデータ
+ * @return Gブレイバーバースト
+ */
+export function restoreGbraverBurst(data: GbraverBurstCoreRestore): GbraverBurstCore {
+  return new GbraverBurstCoreImpl(data.players, data.stateHistory);
+}
+
+/** Gブレイバーバーストコア実装 */
+class GbraverBurstCoreImpl implements GbraverBurstCore {
+  _players: [Player, Player];
+  _stateHistory: GameState[];
+
+  /**
+   * コンストラクタ
+   *
+   * @param players バトルに参加するプレイヤー
+   * @param stateHistory ステートヒストリー
+   */
+  constructor(players: [Player, Player], stateHistory: GameState[]) {
+    this._players = players;
+    this._stateHistory = stateHistory;
+  }
+
+  /** @override */
+  players(): [Player, Player] {
+    return this._players;
+  }
+
+  /** @override */
+  stateHistory(): GameState[] {
+    return this._stateHistory;
+  }
+
+  /** @override */
+  dump(): GbraverBurstCoreRestore {
+    return {players: this._players, stateHistory: this._stateHistory};
+  }
+
+  /** @override */
   progress(commands: [PlayerCommand, PlayerCommand]): GameState[] {
     if (!isAllPlayerEnteredCommand(this._players, commands)) {
       throw new Error('all player not enter command');
