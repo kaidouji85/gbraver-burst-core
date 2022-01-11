b9bed9f3018b0ba2221a40895f68337cb127fb87
Y.Takeuchi <kaidouji85@gmail.com>
2022-01-11 19:02:52 +0900

    ステート更新関数のテストもjestに移行した

--- test/game/game-flow/game-flow.test.js
+++ test/game/game-flow/game-flow.test.js
@@ -1,24 +1,22 @@
 // @flow
 
-import test from 'ava';
 import {GameFlow} from "../../../src/game/game-flow/game-flow";
 import {EMPTY_GAME_STATE} from "../../../src";
 
-test('ステート更新関数に引数には、自身がセットされている', t => {
-  t.plan(1);
-
+test('ステート更新関数の引数には、自身がセットされている', () => {
   const flow = new GameFlow([], EMPTY_GAME_STATE);
   flow.to(v => {
-    t.is(v, flow);
-    return new GameFlow([], EMPTY_GAME_STATE);
+    expect(v).toBe(flow);
+    return v;
   });
 });
 
-test('ステート変更関数の更新結果が次に渡される', t => {
-  t.plan(1);
-
+test('toでchainした結果が、ステート更新関数の引数とセットされている', () => {
   const flowA = new GameFlow([], EMPTY_GAME_STATE);
   const flowB = new GameFlow([], EMPTY_GAME_STATE);
   flowA.to(() => flowB)
-    .to(v => t.is(v, flowB));
+    .to(v => {
+      expect(v).toBe(flowB);
+      return v;
+    });
 });
