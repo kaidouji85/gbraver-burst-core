4df23719082787b955f949be51f33e2c42a54864
Y.Takeuchi <kaidouji85@gmail.com>
2022-03-18 09:46:46 +0900

    命中率のユニットテストを追加した

--- src/score/hit-rate.js
+++ src/score/hit-rate.js
@@ -1,4 +1,4 @@
-//@flow
+// @flow
 
 import type {GameState} from "../state/game-state";
 import type {PlayerId} from "../player/player";
@@ -11,13 +11,13 @@ import type {PlayerId} from "../player/player";
  * @return 計算結果
  */
 export function hitRate(stateHistory: GameState[], attacker: PlayerId): number {
-  if (stateHistory.length <= 0) {
+  const allAttacks = stateHistory
+    .filter(v => v.effect.name === 'Battle' && v.effect.attacker === attacker && v.effect.result.name !== 'Feint');
+  if (allAttacks.length <= 0) {
     return 0;
   }
 
-  const allAttacks = stateHistory
-    .filter(v => v.effect.name === 'Battle' && v.effect.attacker === attacker && v.effect.result.name !== 'Feint');
   const hitBattleResult = ['NormalHit', 'Guard', 'CriticalHit'];
-  const hits = allAttacks.filter(v => v.effect.name === 'Battle' && hitBattleResult.includes(v.effect.name));
+  const hits = allAttacks.filter(v => v.effect.name === 'Battle' && hitBattleResult.includes(v.effect.result.name));
   return hits.length / allAttacks.length;
 }
