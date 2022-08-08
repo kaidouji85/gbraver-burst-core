11c4d19ca110d55fd459000395c9b8d87a859670
kaidouji85 <kaidouji85@gmail.com>
2022-08-08 18:55:51 +0900

    アクティブプレイヤー継続のスナップショットテストを追加した

--- test/game/progress/game-continue-flow.test.js
+++ test/game/progress/game-continue-flow.test.js
@@ -17,17 +17,16 @@ test('ターン交代まで正しく実行できる', () => {
   expect(result).toEqual(snapShot);
 });
 
-/*
 test('連続攻撃を正しく処理できる', () => {
-  const player1 = {...EMPTY_PLAYER_STATE, playerId: 'player1', armdozer: {...EMPTY_ARMDOZER_STATE, maxBattery: 5, battery: 1}};
+  const player1 = {...EMPTY_PLAYER_STATE, playerId: 'player1',
+    armdozer: {...EMPTY_ARMDOZER_STATE, maxBattery: 5, battery: 3, effects: [{type: 'ContinuousActivePlayer', period: {type: 'Permanent'}}]}};
   const player1Command = {type: 'BATTERY_COMMAND', battery: 1};
   const player2 = {...EMPTY_PLAYER_STATE, playerId: 'player2'};
   const player2Command = {type: 'BATTERY_COMMAND', battery: 2};
   const lastState = {...EMPTY_GAME_STATE, players: [player1, player2], activePlayerId: player1.playerId};
   const result = gameContinueFlow(lastState, player1.playerId, player1Command, player2.playerId, player2Command);
-  const snapShotPath = path.join(__dirname, 'game-continue-flow__turn-change.json');
+  const snapShotPath = path.join(__dirname, 'game-continue-flow__continuous-active.json');
   shouldUpdateSnapShot() && exportSnapShotJSON(snapShotPath, result);
   const snapShot = shouldUpdateSnapShot() ? result : importSnapShotJSON(snapShotPath);
   expect(result).toEqual(snapShot);
 });
- */
