0d7527caaa21e0dca90a7ef58c7888754246dcf5
kaidouji85 <kaidouji85@gmail.com>
2022-08-08 17:10:02 +0900

    スナップショット系ヘルパー関数を追加した

---
+++ test/snap-shot.js
@@ -0,0 +1,35 @@
+// @flow
+import * as fs from "fs";
+
+/** JSONスナップショットのエンコード */
+const encode = 'utf-8';
+
+/**
+ * スナップショットを更新するか否かを判定する
+ *
+ * @return 判定結果、trueでスナップショットを更新する
+ */
+export function isUpdateSnapShot(): boolean {
+  return process.env.UPDATE_SNAPSHOT === '1';
+}
+
+/**
+ * 任意のオブジェクトをjson形式でエクスポートする
+ *
+ * @param path エクスポート先のパス
+ * @param json エクスポートするオブジェクト
+ */
+export function exportJSON(path: string, json: Object): void {
+  fs.writeFileSync(path, JSON.stringify(json, null, 2), encode);
+}
+
+/**
+ * スナップショット用JSONをファイルから読み込む
+ *
+ * @param path 読み込み先のパス
+ * @return 読み込み結果
+ */
+export function importJSON(path: string): any {
+  const origin = fs.readFileSync(path, encode);
+  return JSON.parse(origin);
+}
