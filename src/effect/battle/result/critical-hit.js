7f9d8f9a17cde871c32808abf867d97d51228f74
kaidouji85 <kaidouji85@gmail.com>
2022-09-24 12:52:20 +0900

    クリティカルヒットは9999固定ダメージにした

--- src/effect/battle/result/critical-hit.js
+++ src/effect/battle/result/critical-hit.js
@@ -14,18 +14,8 @@ export type CriticalHit = {
 /**
  * クリティカルヒットの戦闘結果を生成する
  *
- * @param attacker 攻撃側プレイヤー
- * @param attackerBattery 攻撃側バッテリー
- * @param defender 防御側プレイヤー
- * @param defenderBattery 防御側バッテリー
  * @return クリティカルヒットの戦闘結果
  */
-export function criticalHit(attacker: PlayerState, attackerBattery: number, defender: PlayerState, defenderBattery: number): CriticalHit {
-  const normalHit = normalHitDamage(attacker, attackerBattery, defender, defenderBattery);
-  const decrease = totalDamageDecrease(defender.armdozer.effects);
-  const damage = toMinDamage(normalHit * 2 - decrease);
-  return {
-    name: 'CriticalHit',
-    damage: damage,
-  };
+export function criticalHit(): CriticalHit {
+  return {name: 'CriticalHit', damage: 9999};
 }
