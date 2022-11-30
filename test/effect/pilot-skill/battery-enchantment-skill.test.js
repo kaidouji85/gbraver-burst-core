9d589ad72ccc14c5189bd805c88d5c3a443c2cac
Y.Takeuchi <kaidouji85@gmail.com>
2022-11-30 05:25:50 +0000

    バッテリー増強スキルを別ファイルに切り出した

--- test/effect/pilot-skill/battery-enchantment-skill.test.js
+++ test/effect/pilot-skill/battery-enchantment-skill.test.js
@@ -6,20 +6,24 @@ import { pilotSkill } from "../../../src/effect/pilot-skill";
 import { EMPTY_GAME_STATE } from "../../../src/empty/game-state";
 import { EMPTY_PILOT } from "../../../src/empty/pilot";
 import { EMPTY_PLAYER_STATE } from "../../../src/empty/player";
-import type { DamageHalvedSkill } from "../../../src/player/pilot";
+import type { BatteryEnchantmentSkill } from "../../../src/player/pilot";
 import {
   exportSnapShotJSON,
   importSnapShotJSON,
   shouldUpdateSnapShot,
 } from "../../snap-shot";
 
-test("ダメージ半減スキルが正しく発動できる", () => {
-  const skill: DamageHalvedSkill = { type: "DamageHalvedSkill", duration: 1 };
+test("バッテリー増強スキルが正しく発動できる", () => {
+  const skill: BatteryEnchantmentSkill = {
+    type: "BatteryEnchantmentSkill",
+    batteryEnchantment: 1,
+    duration: 2,
+  };
   const invoker: PlayerState = {
     ...EMPTY_PLAYER_STATE,
     playerId: "invoker",
     armdozer: { ...EMPTY_PLAYER_STATE.armdozer, effects: [] },
-    pilot: { ...EMPTY_PILOT, skill: skill, enableSkill: true },
+    pilot: { ...EMPTY_PILOT, skill, enableSkill: true },
   };
   const other: PlayerState = { ...EMPTY_PLAYER_STATE, playerId: "other" };
   const state: GameState = {
@@ -28,7 +32,7 @@ test("ダメージ半減スキルが正しく発動できる", () => {
     players: [other, invoker],
   };
   const result = pilotSkill(state, invoker.playerId);
-  const snapShotPath = path.join(__dirname, "pilot-skill__damage-halved.json");
+  const snapShotPath = path.join(__dirname, "battery-enchantment-skill.json");
   shouldUpdateSnapShot() && exportSnapShotJSON(snapShotPath, result);
   const snapShot = shouldUpdateSnapShot()
     ? result
