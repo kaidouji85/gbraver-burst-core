95ba4aa08adc2767161b56a4ea9d5560005fc0d1
Y.Takeuchi <kaidouji85@gmail.com>
2022-11-30 05:21:59 +0000

    攻撃バフスキルを別ファイルに切り出した

--- test/effect/pilot-skill/buff-power-skill.test.js
+++ test/effect/pilot-skill/buff-power-skill.test.js
@@ -6,22 +6,23 @@ import { pilotSkill } from "../../../src/effect/pilot-skill";
 import { EMPTY_GAME_STATE } from "../../../src/empty/game-state";
 import { EMPTY_PILOT } from "../../../src/empty/pilot";
 import { EMPTY_PLAYER_STATE } from "../../../src/empty/player";
-import type { RecoverBatterySkill } from "../../../src/player/pilot";
+import type { BuffPowerSkill } from "../../../src/player/pilot";
 import {
   exportSnapShotJSON,
   importSnapShotJSON,
   shouldUpdateSnapShot,
 } from "../../snap-shot";
 
-test("バッテリー回復スキルを正しく適用できる", () => {
-  const skill: RecoverBatterySkill = {
-    type: "RecoverBatterySkill",
-    recoverBattery: 2,
+test("攻撃バフスキルが正しく発動できる", () => {
+  const skill: BuffPowerSkill = {
+    type: "BuffPowerSkill",
+    buffPower: 600,
+    duration: 2,
   };
   const invoker: PlayerState = {
     ...EMPTY_PLAYER_STATE,
     playerId: "invoker",
-    armdozer: { ...EMPTY_PLAYER_STATE.armdozer, maxBattery: 5, battery: 2 },
+    armdozer: { ...EMPTY_PLAYER_STATE.armdozer, effects: [] },
     pilot: { ...EMPTY_PILOT, skill, enableSkill: true },
   };
   const other: PlayerState = { ...EMPTY_PLAYER_STATE, playerId: "other" };
@@ -31,7 +32,7 @@ test("バッテリー回復スキルを正しく適用できる", () => {
     players: [other, invoker],
   };
   const result = pilotSkill(state, invoker.playerId);
-  const snapShotPath = path.join(__dirname, "recover-battery-skill.json");
+  const snapShotPath = path.join(__dirname, "buff-power-skill.json");
   shouldUpdateSnapShot() && exportSnapShotJSON(snapShotPath, result);
   const snapShot = shouldUpdateSnapShot()
     ? result
--- test/effect/pilot-skill/pilot-skill.test.js
+++ test/effect/pilot-skill/pilot-skill.test.js
@@ -8,7 +8,6 @@ import { EMPTY_PILOT } from "../../../src/empty/pilot";
 import { EMPTY_PLAYER_STATE } from "../../../src/empty/player";
 import type {
   BatteryEnchantmentSkill,
-  BuffPowerSkill,
   DamageHalvedSkill,
 } from "../../../src/player/pilot";
 import {
@@ -16,33 +15,6 @@ import {
   importSnapShotJSON,
   shouldUpdateSnapShot,
 } from "../../snap-shot";
-
-test("攻撃バフスキルが正しく発動できる", () => {
-  const skill: BuffPowerSkill = {
-    type: "BuffPowerSkill",
-    buffPower: 600,
-    duration: 2,
-  };
-  const invoker: PlayerState = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: "invoker",
-    armdozer: { ...EMPTY_PLAYER_STATE.armdozer, effects: [] },
-    pilot: { ...EMPTY_PILOT, skill: skill, enableSkill: true },
-  };
-  const other: PlayerState = { ...EMPTY_PLAYER_STATE, playerId: "other" };
-  const state: GameState = {
-    ...EMPTY_GAME_STATE,
-    activePlayerId: invoker.playerId,
-    players: [other, invoker],
-  };
-  const result = pilotSkill(state, invoker.playerId);
-  const snapShotPath = path.join(__dirname, "pilot-skill__buff-power.json");
-  shouldUpdateSnapShot() && exportSnapShotJSON(snapShotPath, result);
-  const snapShot = shouldUpdateSnapShot()
-    ? result
-    : importSnapShotJSON(snapShotPath);
-  expect(result).toEqual(snapShot);
-});
 
 test("バッテリー増強スキルが正しく発動できる", () => {
   const skill: BatteryEnchantmentSkill = {
