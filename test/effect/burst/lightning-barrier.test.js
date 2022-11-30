50ca1d26b621db64270367c65692e8163095b2bc
Y.Takeuchi <kaidouji85@gmail.com>
2022-11-30 04:50:54 +0000

    電撃バリアをスナップショットテスト化

--- test/effect/burst/lightning-barrier.test.js
+++ test/effect/burst/lightning-barrier.test.js
@@ -1,12 +1,18 @@
 // @flow
 
+import path from "path";
+
 import type { GameState } from "../../../src";
-import { lightningBarrier } from "../../../src/effect/burst/lightning-barrier";
+import { burst } from "../../../src/effect/burst";
 import { EMPTY_ARMDOZER_STATE } from "../../../src/empty/armdozer";
 import { EMPTY_GAME_STATE } from "../../../src/empty/game-state";
 import { EMPTY_PLAYER_STATE } from "../../../src/empty/player";
-import type { LightningBarrier } from "../../../src/player/burst";
 import type { PlayerState } from "../../../src/state/player-state";
+import {
+  exportSnapShotJSON,
+  importSnapShotJSON,
+  shouldUpdateSnapShot,
+} from "../../snap-shot";
 
 test("電撃バリアバーストの適用が正しくできる", () => {
   const burstPlayer: PlayerState = {
@@ -17,53 +23,28 @@ test("電撃バリアバーストの適用が正しくできる", () => {
       enableBurst: true,
       battery: 1,
       maxBattery: 5,
+      effects: [],
+      burst: {
+        type: "LightningBarrier",
+        damage: 1000,
+        duration: 2,
+        recoverBattery: 3,
+      },
     },
   };
   const otherPlayer: PlayerState = {
     ...EMPTY_PLAYER_STATE,
     playerId: "other",
   };
-  const burst: LightningBarrier = {
-    type: "LightningBarrier",
-    damage: 1000,
-    duration: 2,
-    recoverBattery: 3,
-  };
   const lastState: GameState = {
     ...EMPTY_GAME_STATE,
     players: [otherPlayer, burstPlayer],
   };
-
-  const result = lightningBarrier(lastState, burstPlayer.playerId, burst);
-  const expected = {
-    ...lastState,
-    players: [
-      otherPlayer,
-      {
-        ...burstPlayer,
-        armdozer: {
-          ...burstPlayer.armdozer,
-          battery: 4,
-          effects: [
-            ...burstPlayer.armdozer.effects,
-            {
-              type: "TryReflect",
-              damage: 1000,
-              effect: "Lightning",
-              period: {
-                type: "TurnLimit",
-                remainingTurn: 2,
-              },
-            },
-          ],
-        },
-      },
-    ],
-    effect: {
-      name: "BurstEffect",
-      burstPlayer: burstPlayer.playerId,
-      burst: burst,
-    },
-  };
-  expect(result).toEqual(expected);
+  const result = burst(lastState, burstPlayer.playerId);
+  const snapShotPath = path.join(__dirname, "lightning-barrier.json");
+  shouldUpdateSnapShot() && exportSnapShotJSON(snapShotPath, result);
+  const snapShot = shouldUpdateSnapShot()
+    ? result
+    : importSnapShotJSON(snapShotPath);
+  expect(result).toEqual(snapShot);
 });
