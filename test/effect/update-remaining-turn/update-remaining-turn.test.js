375b6b69b52500ae0e13100d71a178d4df9ce992
Y.Takeuchi <kaidouji85@gmail.com>
2022-12-01 09:30:29 +0000

    効果継続ターン更新テストファイル分割

--- test/effect/update-remaining-turn/update-remaining-turn.test.js
+++ test/effect/update-remaining-turn/update-remaining-turn.test.js
@@ -1,85 +1,20 @@
 // @flow
 
-import { updateRemainingTurn } from "../../../src/effect/update-remaning-turn";
-import { EMPTY_ARMDOZER_EFFECT } from "../../../src/empty/amrdozer-effect";
-import { EMPTY_ARMDOZER_STATE } from "../../../src/empty/armdozer";
-import { EMPTY_GAME_STATE } from "../../../src/empty/game-state";
-import { EMPTY_PLAYER_STATE } from "../../../src/empty/player";
-import type {
-  PermanentEffect,
-  TurnLimitEffect,
-} from "../../../src/state/armdozer-effect";
+import path from "path";
 
-const permanent: PermanentEffect = { type: "Permanent" };
-const turnLimit = (turn: number): TurnLimitEffect => ({
-  type: "TurnLimit",
-  remainingTurn: turn,
-});
+import { updateRemainingTurn } from "../../../src/effect/update-remaning-turn";
+import {
+  exportSnapShotJSON,
+  importSnapShotJSON,
+  shouldUpdateSnapShot,
+} from "../../snap-shot";
+import * as Data from "./update-remaining-turn.data";
 
 test("効果継続ターン更新が正しく処理される", () => {
-  const player1 = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: "player1",
-    armdozer: {
-      ...EMPTY_ARMDOZER_STATE,
-      effects: [
-        { ...EMPTY_ARMDOZER_EFFECT, period: turnLimit(1) },
-        { ...EMPTY_ARMDOZER_EFFECT, period: permanent },
-        { ...EMPTY_ARMDOZER_EFFECT, period: turnLimit(4) },
-      ],
-    },
-  };
-  const player2 = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: "player2",
-    armdozer: {
-      ...EMPTY_ARMDOZER_STATE,
-      effects: [
-        { ...EMPTY_ARMDOZER_EFFECT, period: turnLimit(1) },
-        { ...EMPTY_ARMDOZER_EFFECT, period: turnLimit(3) },
-      ],
-    },
-  };
-  const data = {
-    ...EMPTY_GAME_STATE,
-    players: [player1, player2],
-  };
-
-  const result = updateRemainingTurn(data);
-  const expected = {
-    ...data,
-    players: [
-      {
-        ...player1,
-        armdozer: {
-          ...player1.armdozer,
-          effects: [
-            { ...EMPTY_ARMDOZER_EFFECT, period: permanent },
-            { ...EMPTY_ARMDOZER_EFFECT, period: turnLimit(3) },
-          ],
-        },
-      },
-      {
-        ...player2,
-        armdozer: {
-          ...player2.armdozer,
-          effects: [{ ...EMPTY_ARMDOZER_EFFECT, period: turnLimit(2) }],
-        },
-      },
-    ],
-    effect: {
-      name: "UpdateRemainingTurn",
-      endArmdozerEffects: [
-        {
-          playerId: player1.playerId,
-          effect: { ...EMPTY_ARMDOZER_EFFECT, period: turnLimit(0) },
-        },
-        {
-          playerId: player2.playerId,
-          effect: { ...EMPTY_ARMDOZER_EFFECT, period: turnLimit(0) },
-        },
-      ],
-    },
-  };
-  expect(result).toEqual(expected);
+  const { lastState } = Data;
+  const result = updateRemainingTurn(lastState);
+  const snapShotPath = path.join(__dirname, "update-remaining-turn.json");
+  shouldUpdateSnapShot() && exportSnapShotJSON(snapShotPath, result);
+  const snapShot = importSnapShotJSON(snapShotPath);
+  expect(result).toEqual(snapShot);
 });
