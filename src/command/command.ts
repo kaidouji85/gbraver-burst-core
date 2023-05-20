55cc9058d1ee8d45ff57f3af1e10ac794333f166
Y.Takeuchi <kaidouji85@gmail.com>
2023-05-20 17:19:58 +0900

    コマンドパースを追加

--- src/command/command.ts
+++ src/command/command.ts
@@ -1,7 +1,12 @@
+import { z } from "zod";
 import type {BatteryCommand} from "./battery";
 import type {BurstCommand} from "./burst";
 import type {EmptyCommand} from "./empty-command";
 import type {PilotSkillCommand} from "./pilot-skill";
+import {EmptyCommandSchema} from "./empty-command";
+import {BatteryCommandSchema} from "./battery";
+import {BurstCommandSchema} from "./burst";
+import {PilotSkillCommandSchema} from "./pilot-skill";
 
 /** コマンド */
 export type Command =
@@ -10,3 +15,20 @@ export type Command =
   | BurstCommand
   | PilotSkillCommand;
 
+/** コマンド zodスキーマ */
+export const CommandSchema = z.union([
+  EmptyCommandSchema,
+  BatteryCommandSchema,
+  BurstCommandSchema,
+  PilotSkillCommandSchema,
+]);
+
+/**
+ * コマンドにパースする
+ * @param origin パース元
+ * @return パース結果、失敗した場合はnull
+ */
+export function parseCommandSchema(origin: unknown): Command | null {
+  const result = CommandSchema.safeParse(origin);
+  return result.success ? result.data : null;
+}
