915c05655be3c11e025e5810181bcc14d5044be5
kaidouji85 <kaidouji85@gmail.com>
2023-06-08 23:26:07 +0900

    コマンドチェック関数を追加

---
+++ src/game/validation/is-valid-command.ts
@@ -0,0 +1,28 @@
+import {InputCommand} from "../../effect/input-command/input-command";
+import {PlayerCommand} from "../command/player-command";
+import {isCommandEqual} from "../../command/command";
+
+/**
+ * 選択可能なプレイヤーコマンドであるか否かを判定する
+ * @param playerCommand 判定対象のプレイヤーコマンド
+ * @param inputCommand 「コマンド入力」ステート
+ * @return 判定結果、trueで選択可能なプレイヤーコマンド
+ */
+export function isValidCommand(
+  playerCommand: PlayerCommand,
+  inputCommand: InputCommand
+): boolean {
+  const player = inputCommand.players
+    .find((player) => player.playerId === playerCommand.playerId);
+  if (!player) {
+    return false;
+  }
+
+  if (player.selectable) {
+    return player.command
+      .filter(v => isCommandEqual(v, playerCommand.command))
+      .length > 0;
+  }
+
+  return isCommandEqual(player.nextTurnCommand, playerCommand.command);
+}
