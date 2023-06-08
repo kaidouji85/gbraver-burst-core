713132dfafea509f4b2e648298185b9f8b37bc13
kaidouji85 <kaidouji85@gmail.com>
2023-06-08 23:17:19 +0900

    isCommandEqualのユニットテストを追加

---
+++ test/command/is-command-equals.test.ts
@@ -0,0 +1,63 @@
+import {Command, isCommandEqual} from "../../src/command/command";
+
+test("等しい空コマンドと判定できる", () => {
+  expect(isCommandEqual({
+      type: "EMPTY_COMMAND",
+    }, {
+      type: "EMPTY_COMMAND",
+    }
+  )).toBe(true);
+});
+
+test("等しいバッテリーコマンドと判定できる", () => {
+  expect(isCommandEqual({
+      type: "BATTERY_COMMAND",
+      battery: 3,
+    }, {
+      type: "BATTERY_COMMAND",
+      battery: 3,
+    }
+  )).toBe(true);
+});
+
+test("異なるバッテリーコマンドと判定できる", () => {
+  expect(isCommandEqual({
+      type: "BATTERY_COMMAND",
+      battery: 3,
+    }, {
+      type: "BATTERY_COMMAND",
+      battery: 4,
+    }
+  )).toBe(false);
+});
+
+test("等しいバーストコマンドと判定できる", () => {
+  expect(isCommandEqual({
+      type: "BURST_COMMAND",
+    }, {
+      type: "BURST_COMMAND",
+    }
+  )).toBe(true);
+});
+
+test("等しいパイロットスキルコマンドと判定できる", () => {
+  expect(isCommandEqual({
+      type: "PILOT_SKILL_COMMAND",
+    }, {
+      type: "PILOT_SKILL_COMMAND",
+    }
+  )).toBe(true);
+});
+
+test("コマンド種別がことなると別コマンドだと判定できる", () => {
+  const commands: Command[] = [
+    {type: "EMPTY_COMMAND"},
+    {type: "BATTERY_COMMAND", battery: 3},
+    {type: "BURST_COMMAND"},
+  ];
+  commands.forEach((command) => {
+    expect(isCommandEqual({
+      type: "PILOT_SKILL_COMMAND"
+    }, command)).toBe(false);
+  });
+});
