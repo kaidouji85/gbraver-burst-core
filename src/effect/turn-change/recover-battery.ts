d1733fc305354ce50248f2a14cd4421a59212785
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-18 04:42:44 +0000

    新しいターン開始時回復バッテリー計算を実装

--- src/effect/turn-change/recover-battery.ts
+++ src/effect/turn-change/recover-battery.ts
@@ -1,9 +1,11 @@
+import { PlayerState } from "../../state/player-state";
+
 /** ターンチェンジの際に回復するバッテリー */
 export const BATTERY_RECOVERY_VALUE = 3;
 
 /**
+ * @deprecated
  * 回復後のバッテリー値を取得する
- *
  * @param battery 現在のバッテリー値
  * @param maxBattery 最大バッテリー値
  * @param recoveryValue 回復量
@@ -15,4 +17,21 @@ export function turnChangeRecoverBattery(
   recoveryValue: number,
 ): number {
   return Math.min(battery + recoveryValue, maxBattery);
+}
+
+/**
+ * 回復後のバッテリー値を取得する
+ * @param player プレイヤーステート
+ * @return 回復後のバッテリー
+ */
+export function tekitou(
+  player: PlayerState
+): number {
+  const hasBatteryRecoverSkip = player.armdozer.effects
+    .some(v => v.type === 'BatteryRecoverSkip');
+  if (hasBatteryRecoverSkip) {
+    return 0;
+  }
+
+  return Math.min(player.armdozer.battery + BATTERY_RECOVERY_VALUE, player.armdozer.maxBattery);
 }
