3a9bd1121cf2f2aa3b401c6c0e747cc4c43dc3ce
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-18 05:21:28 +0000

    ユニットテストに条件追加

--- test/effect/turn-change/calc-turn-change-recover-battery.test.ts
+++ test/effect/turn-change/calc-turn-change-recover-battery.test.ts
@@ -1,18 +1,38 @@
-import { EMPTY_ARMDOZER_STATE, EMPTY_PLAYER_STATE } from "../../../src";
+import { BatteryRecoverSkip, EMPTY_ARMDOZER_STATE, EMPTY_PLAYER_STATE, PlayerState } from "../../../src";
 import { calcTurnChangeRecoverBattery } from "../../../src/effect/turn-change/recover-battery";
 
-const createPlayer = (battery: number) => ({
+/** 効果 ターン開始時のバッテリー回復をスキップ */
+const batteryRecoverSkip: BatteryRecoverSkip = {
+  type: "BatteryRecoverSkip",
+  period: {
+    type: "Permanent"
+  }
+};
+
+/**
+ * テスト用プレイヤーを生成する
+ * @param battery 現在のバッテリー
+ * @param isBatteryRecoverSkip バッテリー回復スキップの効果を持つか、trueで持つ
+ * @return 生成結果
+ */
+const createPlayer = (
+  battery: number,
+  isBatteryRecoverSkip: boolean
+): PlayerState => ({
   ...EMPTY_PLAYER_STATE,
   playerId: "player1",
   armdozer: {
     ...EMPTY_ARMDOZER_STATE,
     battery,
     maxBattery: 5,
+    effects: isBatteryRecoverSkip
+      ? [ batteryRecoverSkip ]
+      : []
   },
 });
 
-test("回復量だけバッテリーに追加される", () => {
-  const player = createPlayer(1);
+test("ターン開始時のバッテリーが3回復する", () => {
+  const player = createPlayer(1, false);
   expect(calcTurnChangeRecoverBattery(player)).toEqual({
     battery: 4,
     recoverBattery: 3,
@@ -20,8 +40,17 @@ test("回復量だけバッテリーに追加される", () => {
 });
 
 test("バッテリー最大値以上にはならない", () => {
-  expect(calcTurnChangeRecoverBattery(createPlayer(4))).toEqual({
+  const player = createPlayer(4, false);
+  expect(calcTurnChangeRecoverBattery(player)).toEqual({
     battery: 5,
     recoverBattery: 3,
+  });
+});
+
+test("BatteryRecoverSkipが適用されている場合、ターン開始時のバッテリー回復はなし", () => {
+  const player = createPlayer(1, true);
+  expect(calcTurnChangeRecoverBattery(player)).toEqual({
+    battery: 1,
+    recoverBattery: 0,
   });
 });
