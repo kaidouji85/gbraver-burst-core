e34b8b3d44e1c0574d9a316a50c2fb9eec5d0da1
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-18 08:58:42 +0000

    バッテリー回復スキルのユニットテスト修正

--- test/effect/pilot-skill/recover-battery-skill.test.ts
+++ test/effect/pilot-skill/recover-battery-skill.test.ts
@@ -1,6 +1,6 @@
 import path from "path";
 
-import type { GameState, PlayerState } from "../../../src";
+import { EMPTY_ARMDOZER_EFFECT, type GameState, type PlayerState } from "../../../src";
 import { pilotSkill } from "../../../src/effect/pilot-skill";
 import { EMPTY_GAME_STATE } from "../../../src/empty/game-state";
 import { EMPTY_PILOT } from "../../../src/empty/pilot";
@@ -12,23 +12,31 @@ import {
   shouldUpdateSnapShot,
 } from "../../snap-shot";
 
+/** スキル バッテリー回復 */
+const skill: RecoverBatterySkill = {
+  type: "RecoverBatterySkill",
+  recoverBattery: 2,
+};
+
+/** スキル発動者 */
+const invoker: PlayerState = {
+  ...EMPTY_PLAYER_STATE,
+  playerId: "invoker",
+  armdozer: { ...EMPTY_PLAYER_STATE.armdozer, maxBattery: 5, battery: 2, effects: [EMPTY_ARMDOZER_EFFECT, EMPTY_ARMDOZER_EFFECT] },
+  pilot: { ...EMPTY_PILOT, skill, enableSkill: true },
+};
+
+/** それ以外のプレイヤー */
+const other: PlayerState = { ...EMPTY_PLAYER_STATE, playerId: "other" };
+
+/** ゲームステート */
+const state: GameState = {
+  ...EMPTY_GAME_STATE,
+  activePlayerId: invoker.playerId,
+  players: [other, invoker],
+};
+
 test("バッテリー回復スキルを正しく適用できる", () => {
-  const skill: RecoverBatterySkill = {
-    type: "RecoverBatterySkill",
-    recoverBattery: 2,
-  };
-  const invoker: PlayerState = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: "invoker",
-    armdozer: { ...EMPTY_PLAYER_STATE.armdozer, maxBattery: 5, battery: 2 },
-    pilot: { ...EMPTY_PILOT, skill, enableSkill: true },
-  };
-  const other: PlayerState = { ...EMPTY_PLAYER_STATE, playerId: "other" };
-  const state: GameState = {
-    ...EMPTY_GAME_STATE,
-    activePlayerId: invoker.playerId,
-    players: [other, invoker],
-  };
   const result = pilotSkill(state, invoker.playerId);
   const snapShotPath = path.join(__dirname, "recover-battery-skill.json");
   shouldUpdateSnapShot() && exportSnapShotJSON(snapShotPath, result);
