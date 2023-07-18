0ca906c455e243f2864bd216c7e6d8c36ed6389f
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-18 05:14:44 +0000

    不要関数を削除

--- src/effect/turn-change/recover-battery.ts
+++ src/effect/turn-change/recover-battery.ts
@@ -3,22 +3,6 @@ import { PlayerState } from "../../state/player-state";
 /** ターンチェンジの際に回復するバッテリー */
 export const BATTERY_RECOVERY_VALUE = 3;
 
-/**
- * @deprecated
- * 回復後のバッテリー値を取得する
- * @param battery 現在のバッテリー値
- * @param maxBattery 最大バッテリー値
- * @param recoveryValue 回復量
- * @return 回復後のバッテリー
- */
-export function turnChangeRecoverBattery(
-  battery: number,
-  maxBattery: number,
-  recoveryValue: number,
-): number {
-  return Math.min(battery + recoveryValue, maxBattery);
-}
-
 /** 計算結果 */
 type Ret = {
   recoverBattery: number;
