3640210990160075f37d63192956f8b202852e4d
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-18 08:38:28 +0000

    ユニットテスト修正

--- test/effect/pilot-skill/battery-enchantment-skill.test.ts
+++ test/effect/pilot-skill/battery-enchantment-skill.test.ts
@@ -1,6 +1,6 @@
 import path from "path";
 
-import type { GameState, PlayerState } from "../../../src";
+import { EMPTY_ARMDOZER_EFFECT, type GameState, type PlayerState } from "../../../src";
 import { pilotSkill } from "../../../src/effect/pilot-skill";
 import { EMPTY_GAME_STATE } from "../../../src/empty/game-state";
 import { EMPTY_PILOT } from "../../../src/empty/pilot";
@@ -12,24 +12,32 @@ import {
   shouldUpdateSnapShot,
 } from "../../snap-shot";
 
+/** バッテリー増強スキル */
+const skill: BatteryEnchantmentSkill = {
+  type: "BatteryEnchantmentSkill",
+  batteryEnchantment: 1,
+  duration: 2,
+};
+
+/** スキル発動プレイヤー */
+const invoker: PlayerState = {
+  ...EMPTY_PLAYER_STATE,
+  playerId: "invoker",
+  armdozer: { ...EMPTY_PLAYER_STATE.armdozer, effects: [EMPTY_ARMDOZER_EFFECT, EMPTY_ARMDOZER_EFFECT] },
+  pilot: { ...EMPTY_PILOT, skill, enableSkill: true },
+};
+
+/** それ以外のプレイヤー */
+const other: PlayerState = { ...EMPTY_PLAYER_STATE, playerId: "other" };
+
+/** ゲームステート */
+const state: GameState = {
+  ...EMPTY_GAME_STATE,
+  activePlayerId: invoker.playerId,
+  players: [other, invoker],
+};
+
 test("バッテリー増強スキルが正しく発動できる", () => {
-  const skill: BatteryEnchantmentSkill = {
-    type: "BatteryEnchantmentSkill",
-    batteryEnchantment: 1,
-    duration: 2,
-  };
-  const invoker: PlayerState = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: "invoker",
-    armdozer: { ...EMPTY_PLAYER_STATE.armdozer, effects: [] },
-    pilot: { ...EMPTY_PILOT, skill, enableSkill: true },
-  };
-  const other: PlayerState = { ...EMPTY_PLAYER_STATE, playerId: "other" };
-  const state: GameState = {
-    ...EMPTY_GAME_STATE,
-    activePlayerId: invoker.playerId,
-    players: [other, invoker],
-  };
   const result = pilotSkill(state, invoker.playerId);
   const snapShotPath = path.join(__dirname, "battery-enchantment-skill.json");
   shouldUpdateSnapShot() && exportSnapShotJSON(snapShotPath, result);
