dd7d2b1028cca92146712a07d99522b60a0b222f
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-19 05:44:48 +0000

    防御態勢立て直しをReadonly化した

--- src/effect/right-itself/right-itself.ts
+++ src/effect/right-itself/right-itself.ts
@@ -1,15 +1,11 @@
 import type { PlayerId } from "../../player/player";
 import type { BattleResult } from "../battle/result/battle-result";
 
-/**
- * 防御側の体勢を立て直す
- */
-export type RightItself = {
+/** 防御側の体勢を立て直す */
+export type RightItself = Readonly<{
   name: "RightItself";
-
   /** 防御側プレイヤー */
   defender: PlayerId;
-
   /** 戦闘結果 */
   battleResult: BattleResult;
-};
+}>;
