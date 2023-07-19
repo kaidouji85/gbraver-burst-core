54a6288f128b54938b714769d3617df6f4d94639
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-19 05:20:50 +0000

    アームドーザエフェクトをReadonly化

--- src/state/armdozer-effect.ts
+++ src/state/armdozer-effect.ts
@@ -2,16 +2,16 @@
 export type EffectPeriod = TurnLimitEffect | SpecialPeriodEffect;
 
 /** ターン期限付きのエフェクト */
-export type TurnLimitEffect = {
+export type TurnLimitEffect = Readonly<{
   type: "TurnLimit";
   /** 効果持続ターン */
   remainingTurn: number;
-};
+}>;
 
 /** 特殊な有効期限 */
-export type SpecialPeriodEffect = {
+export type SpecialPeriodEffect = Readonly<{
   type: "SpecialPeriod";
-};
+}>;
 
 /**
  * アームドーザに適用される効果
@@ -31,34 +31,34 @@ export type ArmdozerEffect =
  * 何もしない効果
  * 全ての効果か本タイプのプロパティを持つこと
  */
-export type EmptyArmdozerEffect = {
+export type EmptyArmdozerEffect = Readonly<{
   /** 効果判別用のプロパティ */
   type: "Empty";
   /** エフェクト有効期間 */
   period: EffectPeriod;
-};
+}>;
 
 /** 攻撃力補正 */
-export type CorrectPower = {
+export type CorrectPower = Readonly<{
   type: "CorrectPower";
   /** 攻撃力補正値 */
   power: number;
   /** エフェクト有効期間 */
   period: EffectPeriod;
-};
+}>;
 
 /** 攻撃力補正半減 */
-export type HalveCorrectPower = {
+export type HalveCorrectPower = Readonly<{
   type: "HalveCorrectPower";
   /** エフェクト有効期間 */
   period: EffectPeriod;
-};
+}>;
 
 /** ダメージエフェクトの種類 */
 export type ReflectDamageEffect = "Lightning";
 
 /** ダメージ反射 */
-export type TryReflect = {
+export type TryReflect = Readonly<{
   type: "TryReflect";
   /** 反射が成功した場合のダメージ */
   damage: number;
@@ -66,34 +66,34 @@ export type TryReflect = {
   effect: ReflectDamageEffect;
   /** エフェクト有効期間 */
   period: EffectPeriod;
-};
+}>;
 
 /** アクティブプレイヤー継続 */
-export type ContinuousActivePlayer = {
+export type ContinuousActivePlayer = Readonly<{
   type: "ContinuousActivePlayer";
   /** エフェクト有効期間 */
   period: EffectPeriod;
-};
+}>;
 
 /** バッテリー補正 */
-export type BatteryCorrection = {
+export type BatteryCorrection = Readonly<{
   type: "BatteryCorrection";
   /** バッテリー補正値 */
   batteryCorrection: number;
   /** エフェクト有効期間 */
   period: EffectPeriod;
-};
+}>;
 
 /** ダメージ半減 */
-export type DamageHalved = {
+export type DamageHalved = Readonly<{
   type: "DamageHalved";
   /** エフェクト有効期間 */
   period: EffectPeriod;
-};
+}>;
 
 /** ターン開始時のバッテリー回復をスキップ */
-export type BatteryRecoverSkip = {
+export type BatteryRecoverSkip = Readonly<{
   type: "BatteryRecoverSkip";
   /** エフェクト有効期間 */
   period: EffectPeriod;
-};
+}>;
