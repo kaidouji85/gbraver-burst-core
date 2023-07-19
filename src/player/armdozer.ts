57b73914723f34a0ac6acb9cbd875e50cbbf9da6
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-19 05:11:41 +0000

    アームドーザをReadonly化

--- src/player/armdozer.ts
+++ src/player/armdozer.ts
@@ -8,28 +8,22 @@ export type ArmDozerId = string;
  *
  * @typeparam {X} バースト
  */
-export type ArmdozerX<X> = {
+export type ArmdozerX<X> = Readonly<{
   /** アームドーザID */
   id: ArmDozerId;
-
   /** 名前 */
   name: string;
-
   /** 最大HP */
   maxHp: number;
-
   /** 最大バッテリー */
   maxBattery: number;
-
   /** 攻撃 */
   power: number;
-
   /** スピード */
   speed: number;
-
   /** バースト */
   burst: X;
-};
+}>;
 
 /** アームドーザ基本情報 */
 export type Armdozer = ArmdozerX<Burst>;
