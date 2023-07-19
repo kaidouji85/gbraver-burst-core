a5d512c109f60d33a30a794fcd4b87506d2aa6ce
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-19 05:49:09 +0000

    EmptyCommandをReadonly化した

--- src/command/empty-command.ts
+++ src/command/empty-command.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 /** コマンド未入力状態 */
-export type EmptyCommand = {
+export type EmptyCommand = Readonly<{
   type: "EMPTY_COMMAND";
-};
+}>;
 
 /** コマンド未入力状態 zodスキーマ */
 export const EmptyCommandSchema = z.object({
