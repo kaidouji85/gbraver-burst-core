937537e4d0dc8d3c64b50829de617644fb215fd4
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-19 02:57:35 +0000

    lint、コードフォーマット

--- test/effect/battle/result/normal-hit.test.ts
+++ test/effect/battle/result/normal-hit.test.ts
@@ -8,36 +8,53 @@ import { EMPTY_ARMDOZER_STATE } from "../../../../src/empty/armdozer";
 import { EMPTY_PLAYER_STATE } from "../../../../src/empty/player";
 import type { PlayerState } from "../../../../src/state/player-state";
 
+/** 攻撃プレイヤーのパラメータ */
 type AttackerParams = {
-  power: number,
-  effects?: ArmdozerEffect[],
-}
-
-const createAttacker = ({power, effects}: AttackerParams): PlayerState => ({
-  ...EMPTY_PLAYER_STATE,
-    playerId: "attacker",
-    armdozer: { ...EMPTY_ARMDOZER_STATE, power, effects: effects ?? [] },
-});
-
-type DefenderParams = {
+  /** 攻撃力 */
+  power: number;
+  /** 効果 */
   effects?: ArmdozerEffect[];
 };
 
-const createDefender = ({effects}: DefenderParams): PlayerState => ({
-  ...EMPTY_PLAYER_STATE, 
-  playerId: "player2",
-  armdozer: { ...EMPTY_ARMDOZER_STATE, effects: effects ?? [] }
+/**
+ * 攻撃プレイヤーを生成する
+ * @param params パラメータ
+ * @return 生成結果
+ */
+const createAttacker = (params: AttackerParams): PlayerState => ({
+  ...EMPTY_PLAYER_STATE,
+  playerId: "attacker",
+  armdozer: { ...EMPTY_ARMDOZER_STATE, ...params },
 });
 
-const correctPower = {
-  ...EMPTY_CORRECT_POWER,
-  power: 1000
+/** 防御側プレイヤーのパラメータ */
+type DefenderParams = {
+  /** 効果 */
+  effects?: ArmdozerEffect[];
 };
 
+/**
+ * 防御側プレイヤーを生成する
+ * @param params パラメータ
+ * @return 生成結果
+ */
+const createDefender = (params: DefenderParams): PlayerState => ({
+  ...EMPTY_PLAYER_STATE,
+  playerId: "player2",
+  armdozer: { ...EMPTY_ARMDOZER_STATE, ...params },
+});
+
+/** 攻撃補正効果 */
+const correctPower = {
+  ...EMPTY_CORRECT_POWER,
+  power: 1000,
+};
+
+/** ダメージ半減効果 */
 const damageHalved = EMPTY_DAMAGE_HALVED;
 
 test("通常ヒットのダメージ計算が正しい", () => {
-  const attacker = createAttacker({power: 2000});
+  const attacker = createAttacker({ power: 2000 });
   const defender = createDefender({});
   expect(normalHit(attacker, 5, defender, 2)).toEqual({
     name: "NormalHit",
@@ -46,7 +63,7 @@ test("通常ヒットのダメージ計算が正しい", () => {
 });
 
 test("攻撃補正が正しく適用される", () => {
-  const attacker = createAttacker({power: 2000, effects: [correctPower]});
+  const attacker = createAttacker({ power: 2000, effects: [correctPower] });
   const defender = createDefender({});
   expect(normalHit(attacker, 5, defender, 2)).toEqual({
     name: "NormalHit",
@@ -55,8 +72,8 @@ test("攻撃補正が正しく適用される", () => {
 });
 
 test("ダメージ半減が正しく適用される", () => {
-  const attacker = createAttacker({power: 2000});
-  const defender = createDefender({effects: [damageHalved]});
+  const attacker = createAttacker({ power: 2000 });
+  const defender = createDefender({ effects: [damageHalved] });
   expect(normalHit(attacker, 5, defender, 2)).toEqual({
     name: "NormalHit",
     damage: 1100,
@@ -64,8 +81,8 @@ test("ダメージ半減が正しく適用される", () => {
 });
 
 test("攻撃補正 -> ダメージ半減の順番で計算する", () => {
-  const attacker = createAttacker({power: 2000, effects: [correctPower]});
-  const defender = createDefender({effects: [damageHalved]});
+  const attacker = createAttacker({ power: 2000, effects: [correctPower] });
+  const defender = createDefender({ effects: [damageHalved] });
   expect(normalHit(attacker, 5, defender, 2)).toEqual({
     name: "NormalHit",
     damage: 1600,
