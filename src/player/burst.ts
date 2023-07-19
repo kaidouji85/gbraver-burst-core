25a768a08fa7415d43811ac5b04e05b774e94b27
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-19 05:14:34 +0000

    バースト系データ型をReadonly化

--- src/player/burst.ts
+++ src/player/burst.ts
@@ -9,47 +9,42 @@ export type Burst =
 /**
  * 全バースト共通で利用するバッテリー回復プロパティ
  */
-export type BurstRecoverBattery = {
+export type BurstRecoverBattery = Readonly<{
   /** バッテリー回復量 */
   recoverBattery: number;
-};
+}>;
 
 /** バッテリー回復 */
-export type RecoverBattery = BurstRecoverBattery & {
+export type RecoverBattery = BurstRecoverBattery & Readonly<{
   type: "RecoverBattery";
-};
+}>;
 
 /** 攻撃力バフ */
-export type BuffPower = BurstRecoverBattery & {
+export type BuffPower = BurstRecoverBattery & Readonly<{
   type: "BuffPower";
-
   /** 攻撃力アップ */
   buffPower: number;
-
   /** バフ継続ターン数 */
   duration: number;
-};
+}>;
 
 /** 電撃バリア */
-export type LightningBarrier = BurstRecoverBattery & {
+export type LightningBarrier = BurstRecoverBattery & Readonly<{
   type: "LightningBarrier";
-
   /** 電撃ダメージ */
   damage: number;
-
   /** バリア継続ターン数 */
   duration: number;
-};
+}>;
 
 /** 連続攻撃 */
-export type ContinuousAttack = BurstRecoverBattery & {
+export type ContinuousAttack = BurstRecoverBattery & Readonly<{
   type: "ContinuousAttack";
-};
+}>;
 
 /** バッテリーリミットブレイク */
-export type BatteryLimitBreak = BurstRecoverBattery & {
+export type BatteryLimitBreak = BurstRecoverBattery & Readonly<{
   type: "BatteryLimitBreak";
-
   /** バースト後の最大バッテリー */
   maxBattery: number;
-};
+}>;
