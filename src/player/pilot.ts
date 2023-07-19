a6a5aeb9722ff7d1a82dd0da57b9bc7166015e60
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-19 05:16:13 +0000

    パイロット系のデータ型をReadonly化

--- src/player/pilot.ts
+++ src/player/pilot.ts
@@ -10,45 +10,46 @@ export type PilotSkill =
   | BatteryBoostSkill;
 
 /** パイロットスキル バッテリー回復 */
-export type RecoverBatterySkill = {
+export type RecoverBatterySkill = Readonly<{
   type: "RecoverBatterySkill";
+  /** バッテリー回復量 */
   recoverBattery: number;
-};
+}>;
 
 /** パイロットスキル 攻撃バフ */
-export type BuffPowerSkill = {
+export type BuffPowerSkill = Readonly<{
   type: "BuffPowerSkill";
   /** 攻撃力アップ */
   buffPower: number;
   /** バフ継続ターン数 */
   duration: number;
-};
+}>;
 
 /** バッテリー増強スキル */
-export type BatteryEnchantmentSkill = {
+export type BatteryEnchantmentSkill = Readonly<{
   type: "BatteryEnchantmentSkill";
   /** バッテリー増強値 */
   batteryEnchantment: number;
   /** 継続ターン数 */
   duration: number;
-};
+}>;
 
 /** ダメージ半減スキル */
-export type DamageHalvedSkill = {
+export type DamageHalvedSkill = Readonly<{
   type: "DamageHalvedSkill";
   /** 継続ターン数 */
   duration: number;
-};
+}>;
 
 /**
  * バッテリーブーストスキル
  * バッテリーを大幅回復できるが、次の自分ターン開始時のバッテリー回復がスキップされる
  */
-export type BatteryBoostSkill = {
+export type BatteryBoostSkill = Readonly<{
   type: "BatteryBoostSkill";
   /** バッテリー回復量 */
   recoverBattery: number;
-};
+}>;
 
 /** パイロット */
 export type Pilot = PilotX<PilotSkill>;
@@ -57,11 +58,11 @@ export type Pilot = PilotX<PilotSkill>;
  * パイロット
  * @template X パイロットスキル
  */
-export type PilotX<X> = {
+export type PilotX<X> = Readonly<{
   /** ID */
   id: PilotId;
   /** パイロット名 */
   name: string;
   /** スキル */
   skill: X;
-};
+}>;
