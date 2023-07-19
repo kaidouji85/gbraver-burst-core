5dc1b5fa4294c7bffe7dc8ee4f0c3137a943b5ba
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-19 05:44:06 +0000

    効果継続ターン数更新をReadonly化した

--- src/effect/update-remaning-turn/update-remaining-turn.ts
+++ src/effect/update-remaning-turn/update-remaining-turn.ts
@@ -2,20 +2,16 @@ import type { PlayerId } from "../../player/player";
 import type { ArmdozerEffect } from "../../state/armdozer-effect";
 
 /** 終了したアームドーザ効果 */
-export type EndArmdozerEffect = {
+export type EndArmdozerEffect = Readonly<{
   /** プレイヤーID */
   playerId: PlayerId;
-
   /** 終了したエフェクト */
   effect: ArmdozerEffect;
-};
+}>;
 
-/**
- * 効果継続ターン数を更新する
- */
-export type UpdateRemainingTurn = {
+/** 効果継続ターン数を更新する */
+export type UpdateRemainingTurn = Readonly<{
   name: "UpdateRemainingTurn";
-
   /** 終了したアームドーザ効果 */
   endArmdozerEffects: EndArmdozerEffect[];
-};
+}>;
