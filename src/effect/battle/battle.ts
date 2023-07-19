425f736d2bb9a81bfdd4a172192b5c875e71b4dd
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-19 05:42:02 +0000

    戦闘をReadonly化

--- src/effect/battle/battle.ts
+++ src/effect/battle/battle.ts
@@ -2,22 +2,18 @@ import type { PlayerId } from "../../player/player";
 import type { BattleResult } from "./result/battle-result";
 
 /**
- * 戦闘
- *
- * @typeparam {X} 戦闘結果
+ * 戦闘（型指定あり）
+ * @template X 戦闘結果
  */
-export type BattleX<X> = {
+export type BattleX<X> = Readonly<{
   name: "Battle";
-
   /** 攻撃側プレイヤー */
   attacker: PlayerId;
-
   /** 死亡フラグ、trueで防御側が死亡 */
   isDeath: boolean;
-
   /** 戦闘結果 */
   result: X;
-};
+}>;
 
 /** 戦闘 */
 export type Battle = BattleX<BattleResult>;
