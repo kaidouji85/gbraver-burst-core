d9c7989d4ece6e54b781969c87b0c4db0d66ae33
Y.Takeuchi <kaidouji85@gmail.com>
2023-07-19 05:46:10 +0000

    パイロットスキル発動をReadonly化した

--- src/effect/pilot-skill/pilot-skill-effect.ts
+++ src/effect/pilot-skill/pilot-skill-effect.ts
@@ -1,14 +1,17 @@
 import type { PilotSkill } from "../../player/pilot";
 import type { PlayerId } from "../../player/player";
 
-/** パイロットスキル発動 */
-export type PilotSkillEffect = PilotSkillEffectX<PilotSkill>;
-
-/** パイロットスキル発動 */
-export type PilotSkillEffectX<SKILL extends PilotSkill> = {
+/** 
+ * パイロットスキル発動（型指定あり）
+ * @template SKILL パイロットスキル
+ */
+export type PilotSkillEffectX<SKILL extends PilotSkill> = Readonly<{
   name: "PilotSkillEffect";
   /** パイロットスキル発動者 */
   invokerId: PlayerId;
   /** 発動スキル */
   skill: SKILL;
-};
+}>;
+
+/** パイロットスキル発動 */
+export type PilotSkillEffect = PilotSkillEffectX<PilotSkill>;
