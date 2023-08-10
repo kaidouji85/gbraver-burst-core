badaf83875e04b4e1c1c69fcafd21f891756cafd
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-10 07:41:47 +0000

    BurstRecoverBatteryをzod化

--- src/player/burst.ts
+++ src/player/burst.ts
@@ -1,14 +1,36 @@
+import { z } from "zod";
+
 /** 全バースト共通で利用するバッテリー回復プロパティ */
 export type BurstRecoverBattery = Readonly<{
   /** バッテリー回復量 */
   recoverBattery: number;
 }>;
 
+/** BurstRecoverBattery zodスキーマ */
+export const BurstRecoverBatterySchema = z.object({
+  recoverBattery: z.number(),
+});
+
+/**
+ * 任意オブジェクトをBurstRecoverBatteryにパースする
+ * @param origin パース元
+ * @return パース結果、パースに失敗した場合はnull
+ */
+export const parseBurstRecoverBattery = (origin: unknown): BurstRecoverBattery | null => {
+  const result = BurstRecoverBatterySchema.safeParse(origin);
+  return result.success ? result.data : null;
+}
+
 /** バッテリー回復 */
 export type RecoverBattery = BurstRecoverBattery &
   Readonly<{
     type: "RecoverBattery";
   }>;
+
+/** バッテリー回復 zodスキーマ */
+export const RecoverBatterySchema = BurstRecoverBatterySchema.extend({
+  type: z.literal("RecoverBattery"),
+});
 
 /** 攻撃力バフ */
 export type BuffPower = BurstRecoverBattery &
