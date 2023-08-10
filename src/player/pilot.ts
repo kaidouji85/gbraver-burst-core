98b3a53da409f31b10a6235bc2c7da7181bb0450
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-10 08:39:48 +0000

    BuffPowerSkillをzod化した

--- src/player/pilot.ts
+++ src/player/pilot.ts
@@ -48,6 +48,23 @@ export type BuffPowerSkill = Readonly<{
   duration: number;
 }>;
 
+/** BuffPowerSkill zodスキーマ */
+export const BuffPowerSkillSchema = z.object({
+  type: z.literal("BuffPowerSkill"),
+  buffPower: z.number(),
+  duration: z.number(),
+});
+
+/**
+ * 任意オブジェクトをBuffPowerSkillにパースする
+ * @param origin パース元
+ * @return パース結果、パースできない場合はnull
+ */
+export const parseBuffPowerSkill = (origin: unknown): BuffPowerSkill | null => {
+  const result = BuffPowerSkillSchema.safeParse(origin);
+  return result.success ? result.data : null;
+};
+
 /** バッテリー増強スキル */
 export type BatteryEnchantmentSkill = Readonly<{
   type: "BatteryEnchantmentSkill";
