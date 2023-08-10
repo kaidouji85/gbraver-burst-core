3752842e121e22c2bc90846004a58530ca87d55a
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-10 07:56:06 +0000

    フォルダ構成変更をやめた

--- src/player/burst.ts
+++ src/player/burst.ts
@@ -1,6 +1,25 @@
 import { z } from "zod";
 
-import { BurstRecoverBattery, BurstRecoverBatterySchema } from "./burst-recover-battery";
+/** 全バースト共通で利用するバッテリー回復プロパティ */
+export type BurstRecoverBattery = Readonly<{
+  /** バッテリー回復量 */
+  recoverBattery: number;
+}>;
+
+/** BurstRecoverBattery zodスキーマ */
+export const BurstRecoverBatterySchema = z.object({
+  recoverBattery: z.number(),
+});
+
+/**
+ * 任意オブジェクトをBurstRecoverBatteryにパースする
+ * @param origin パース元
+ * @return パース結果、パースに失敗した場合はnull
+ */
+export const parseBurstRecoverBattery = (origin: unknown): BurstRecoverBattery | null => {
+  const result = BurstRecoverBatterySchema.safeParse(origin);
+  return result.success ? result.data : null;
+}
 
 /** バッテリー回復 */
 export type RecoverBattery = BurstRecoverBattery &
