9cd6d1a3484b5707e45b88b9a25028152b28a8cd
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-10 10:01:18 +0000

    parseBatteryBoostSkillのユニットテストを追加

---
+++ test/player/parse-battery-boost-skill.test.ts
@@ -0,0 +1,17 @@
+import { BatteryBoostSkill, parseBatteryBoostSkill } from "../../src";
+
+test("BatteryBoostSkillはパースできる", () => {
+  const data: BatteryBoostSkill = {
+    type: "BatteryBoostSkill",
+    recoverBattery: 5,
+  };
+  expect(parseBatteryBoostSkill(data)).toEqual(data);
+});
+
+test("BatteryBoostSkill以外はnullを返す", () => {
+  const data = {
+    type: "BatteryBoost",
+    recover: 5,
+  };
+  expect(parseBatteryBoostSkill(data)).toBe(null);
+});
