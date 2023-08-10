212a3caa33151bfb83cf80d1ebee447b11cd80b3
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-10 09:24:41 +0000

    parseBuffPowerのユニットテストを追加

---
+++ test/player/parse-buff-power.test.ts
@@ -0,0 +1,21 @@
+import { BuffPower, parseBuffPower } from "../../src";
+
+test("BuffPowerはパースできる", () => {
+  const data: BuffPower = {
+    type: "BuffPower",
+    buffPower: 1000,
+    duration: 3,
+    recoverBattery: 3,
+  };
+  expect(parseBuffPower(data)).toEqual(data);
+});
+
+test("BuffPower以外はnullを返す", () => {
+  const data = {
+    type: "Buff",
+    power: 1000,
+    effectDuration: 3,
+    battery: 3,
+  };
+  expect(parseBuffPower(data)).toBe(null);
+});
