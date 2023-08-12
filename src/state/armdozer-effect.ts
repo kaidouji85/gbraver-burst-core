4c4fa56148611444ea11cd1986c98cebaa4f8050
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-12 17:25:52 +0900

    ReflectDamageEffectをzod化

--- src/state/armdozer-effect.ts
+++ src/state/armdozer-effect.ts
@@ -141,6 +141,19 @@ export const parseHalveCorrectPower = (origin: unknown): HalveCorrectPower | nul
 /** ダメージエフェクトの種類 */
 export type ReflectDamageEffect = "Lightning";
 
+/** ReflectDamageEffect zodスキーマ */
+export const ReflectDamageEffectSchema = z.literal("Lightning");
+
+/**
+ * 任意オブジェクトをReflectDamageEffectにパースする
+ * @param origin パース元
+ * @return パース結果、パースできない場合はnull
+ */
+export const parseReflectDamageEffect = (origin: unknown): ReflectDamageEffect | null => {
+  const result = ReflectDamageEffectSchema.safeParse(origin);
+  return result.success ? result.data : null;
+}
+
 /** ダメージ反射 */
 export type TryReflect = Readonly<{
   type: "TryReflect";
