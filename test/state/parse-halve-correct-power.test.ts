a0991a097378c45bf0aef635e57b4d561ae925f5
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-12 22:58:32 +0900

    parseHalveCorrectPowerのユニットテストを追加

---
+++ test/state/parse-halve-correct-power.test.ts
@@ -0,0 +1,25 @@
+import {HalveCorrectPower, parseHalveCorrectPower} from "../../src/state/armdozer-effect";
+
+/** 有効なHalveCorrectPower */
+const halveCorrectPower: HalveCorrectPower = {
+  type: "HalveCorrectPower",
+  period: {
+    type: "TurnLimit",
+    remainingTurn: 1,
+  }
+};
+
+test("HalveCorrectPowerはパースできる", () => {
+  expect(parseHalveCorrectPower(halveCorrectPower)).toEqual(halveCorrectPower);
+});
+
+test("HalveCorrectPower以外はパースできない", () => {
+  const data = {
+    type: "HalvePower",
+    period: {
+      type: "TurnLimit",
+      remaining: 1,
+    }
+  };
+  expect(parseHalveCorrectPower(data)).toBe(null);
+});
