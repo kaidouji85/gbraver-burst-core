2bf2ced9db7eb2ef2095a348aed13427e2fe3573
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-12 17:28:13 +0900

    ContinuousActivePlayerをzod化

--- src/state/armdozer-effect.ts
+++ src/state/armdozer-effect.ts
@@ -190,6 +190,22 @@ export type ContinuousActivePlayer = Readonly<{
   period: EffectPeriod;
 }>;
 
+/** ContinuousActivePlayer zodスキーマ */
+export const ContinuousActivePlayerSchema = z.object({
+  type: z.literal("ContinuousActivePlayer"),
+  period: EffectPeriodSchema,
+});
+
+/**
+ * 任意オブジェクトをContinuousActivePlayerにパースする
+ * @param origin パース元
+ * @return パース結果、パースできない場合はnull
+ */
+export const parseContinuousActivePlayer = (origin: unknown): ContinuousActivePlayer | null => {
+  const result = ContinuousActivePlayerSchema.safeParse(origin);
+  return result.success ? result.data : null;
+}
+
 /** バッテリー補正 */
 export type BatteryCorrection = Readonly<{
   type: "BatteryCorrection";
