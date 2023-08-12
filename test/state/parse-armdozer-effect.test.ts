c657ad108900ad9993028df4e73be44ad90e5b5c
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-12 23:30:15 +0900

    parseArmdozerEffectのユニットテストを追加

---
+++ test/state/parse-armdozer-effect.test.ts
@@ -0,0 +1,81 @@
+import {ArmdozerEffect, parseArmdozerEffect} from "../../src";
+
+/** 有効なArmdozerEffect */
+const armdozerEffects: ArmdozerEffect[] = [
+  {
+    type: 'Empty',
+    period: {
+      type: 'TurnLimit',
+      remainingTurn: 1,
+    }
+  },
+  {
+    type: "CorrectPower",
+    power: 1000,
+    period: {
+      type: "TurnLimit",
+      remainingTurn: 2,
+    }
+  },
+  {
+    type: "HalveCorrectPower",
+    period: {
+      type: "TurnLimit",
+      remainingTurn: 2,
+    }
+  },
+  {
+    type: "TryReflect",
+    effect: "Lightning",
+    damage: 2000,
+    period: {
+      type: "TurnLimit",
+      remainingTurn: 2,
+    }
+  },
+  {
+    type: "ContinuousActivePlayer",
+    period: {
+      type: "SpecialPeriod",
+    }
+  },
+  {
+    type: "BatteryCorrection",
+    batteryCorrection: 1000,
+    period: {
+      type: "TurnLimit",
+      remainingTurn: 2,
+    }
+  },
+  {
+    type: "DamageHalved",
+    period: {
+      type: "TurnLimit",
+      remainingTurn: 1,
+    }
+  },
+  {
+    type: "BatteryRecoverSkip",
+    period: {
+      type: "SpecialPeriod",
+    }
+  }
+];
+
+test("ArmdozerEffectはパースできる", () => {
+  armdozerEffects.forEach(effect => {
+    expect(parseArmdozerEffect(effect)).toEqual(effect);
+  });
+});
+
+test("ArmdozerEffect以外はパースできない", () => {
+  const data = {
+    type: "CorrectPower",
+    powerUp: 1000,
+    period: {
+      type: "TurnLimit",
+      remaining: 1,
+    }
+  };
+  expect(parseArmdozerEffect(data)).toBe(null);
+});
