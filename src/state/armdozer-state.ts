397b929bc18d5a57132f961e74c9e92b5fe2b4be
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-16 05:43:27 +0000

    ArmdozerStateSchemaをzod化

--- src/state/armdozer-state.ts
+++ src/state/armdozer-state.ts
@@ -1,6 +1,8 @@
-import type { Armdozer, ArmdozerX } from "../player/armdozer";
-import type { Burst } from "../player/burst";
-import type { ArmdozerEffect } from "./armdozer-effect";
+import { z } from "zod";
+
+import { Armdozer, ArmdozerSchema, ArmdozerX } from "../player/armdozer";
+import { Burst } from "../player/burst";
+import { ArmdozerEffect, ArmdozerEffectSchema } from "./armdozer-effect";
 
 /**
  * アームドーザステート（型指定あり）
@@ -20,6 +22,24 @@ export type ArmdozerStateX<X> = ArmdozerX<X> &
 
 /** アームドーザステート */
 export type ArmdozerState = ArmdozerStateX<Burst>;
+
+/** ArmdozerState zodスキーマ */
+export const ArmdozerStateSchema = ArmdozerSchema.extend({
+  hp: z.number(),
+  battery: z.number(),
+  enableBurst: z.boolean(),
+  effects: z.array(ArmdozerEffectSchema),
+});
+
+/**
+ * 任意オブジェクトをArmdozerStateにパースする
+ * @param origin パース元
+ * @return パース結果、パースできない場合はnull
+ */
+export const parseArmDozerState = (origin: unknown): ArmdozerState | null => {
+  const result = ArmdozerStateSchema.safeParse(origin);
+  return result.success ? result.data : null;
+};
 
 /**
  * アームドーザ基本ステータスから戦闘状態を生成する
