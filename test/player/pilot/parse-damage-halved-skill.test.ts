1c4ae05840cd59471173d5daf8d18911528f4a65
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-17 10:59:34 +0000

    parseDamageHalvedSkillも同様の対応

--- test/player/pilot/parse-damage-halved-skill.test.ts
+++ test/player/pilot/parse-damage-halved-skill.test.ts
@@ -1,11 +1,19 @@
 import { DamageHalvedSkill, parseDamageHalvedSkill } from "../../../src";
 
+/** 有効なDamageHalvedSkill */
+const damageHalvedSkill: DamageHalvedSkill = {
+  type: "DamageHalvedSkill",
+  duration: 1,
+};
+
 test("DamageHalvedSkillはパースできる", () => {
-  const data: DamageHalvedSkill = {
-    type: "DamageHalvedSkill",
-    duration: 1,
-  };
-  expect(parseDamageHalvedSkill(data)).toEqual(data);
+  expect(parseDamageHalvedSkill(damageHalvedSkill)).toEqual(damageHalvedSkill);
+});
+
+test("文字からJSONパースしたオブジェクトでも、正しくパースできる", () => {
+  const str = JSON.stringify(damageHalvedSkill);
+  const data = JSON.parse(str);
+  expect(parseDamageHalvedSkill(data)).toEqual(damageHalvedSkill);
 });
 
 test("DamageHalvedSkill以外はnullを返す", () => {
