477027719905e44f15cbe846ef37802c0d4fa3a1
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-17 10:06:45 +0000

    parseBattleResultのユニットテストを追加

---
+++ test/effect/battle/result/parse-battle-results.test.ts
@@ -0,0 +1,31 @@
+import { BattleResult, parseBattleResult } from "../../../../src";
+
+/** 有効なBattleResult */
+const battleResults: BattleResult[] = [
+  {
+    name: "NormalHit",
+    damage: 2200,
+  }
+];
+
+test("BattleResultはパースできる", () => {
+  battleResults.forEach(battleResult => {
+    expect(parseBattleResult(battleResult)).toEqual(battleResult);
+  });
+});
+
+test("文字からJSONパースしたオブジェクトでも、正しくパースできる", () => {
+  battleResults.forEach(battleResult => {
+    const str = JSON.stringify(battleResult);
+    const data = JSON.parse(str);
+    expect(parseBattleResult(data)).toEqual(data);
+  });
+});
+
+test("BattleResult以外はパースできない", () => {
+  const data = {
+    type: "NormalHit",
+    damaged: 2300,
+  };
+  expect(parseBattleResult(data)).toBeNull();
+});
