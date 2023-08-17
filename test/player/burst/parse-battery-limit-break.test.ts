5af6050cd23dbd2b388f0542e5b2778ee2a6892f
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-17 10:44:44 +0000

    parseBatteryLimitBreakも同様の対応

--- test/player/burst/parse-battery-limit-break.test.ts
+++ test/player/burst/parse-battery-limit-break.test.ts
@@ -1,12 +1,20 @@
 import { BatteryLimitBreak, parseBatteryLimitBreak } from "../../../src";
 
+/** 有効なBatteryLimitBreak */
+const batteryLimitBreak: BatteryLimitBreak = {
+  type: "BatteryLimitBreak",
+  recoverBattery: 8,
+  maxBattery: 8,
+}
+
 test("BatteryLimitBreakはパースできる", () => {
-  const data: BatteryLimitBreak = {
-    type: "BatteryLimitBreak",
-    recoverBattery: 8,
-    maxBattery: 8,
-  };
-  expect(parseBatteryLimitBreak(data)).toEqual(data);
+  expect(parseBatteryLimitBreak(batteryLimitBreak)).toEqual(batteryLimitBreak);
+});
+
+test("文字からJSONパースしたオブジェクトでも、正しくパースできる", () => {
+  const str = JSON.stringify(batteryLimitBreak);
+  const data = JSON.parse(str);
+  expect(parseBatteryLimitBreak(data)).toEqual(batteryLimitBreak);
 });
 
 test("BatteryLimitBreak以外はnullを返す", () => {
