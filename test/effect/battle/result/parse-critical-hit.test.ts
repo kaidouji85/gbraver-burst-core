418309a1ca16e54ffa7cb7dcde413717e5fb0598
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-17 09:50:10 +0000

    parseCriticalHitのユニットテストを追加

---
+++ test/effect/battle/result/parse-critical-hit.test.ts
@@ -0,0 +1,25 @@
+import { CriticalHit, parseCriticalHit } from "../../../../src";
+
+/** 有効なCriticalHit */
+const criticalHit: CriticalHit = {
+  name: "CriticalHit",
+  damage: 9999,
+};
+
+test("CriticalHitはパースできる", () => {
+  expect(parseCriticalHit(criticalHit)).toEqual(criticalHit);
+});
+
+test("文字からJSONパースしたオブジェクトでも、正しくパースできる", () => {
+  const str = JSON.stringify(criticalHit);
+  const data = JSON.parse(str);
+  expect(parseCriticalHit(data)).toEqual(data);
+});
+
+test("CriticalHit以外はパースできない", () => {
+  const data = {
+    type: "CriticalHit",
+    damaged: 9999,
+  };
+  expect(parseCriticalHit(data)).toBeNull();
+});
