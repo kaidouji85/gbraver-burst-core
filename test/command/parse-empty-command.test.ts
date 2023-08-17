0953bcdca935fe5287546fb69825d18790d67e96
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-17 10:34:28 +0000

    parseEmptyCommandも同様の対応

--- test/command/parse-empty-command.test.ts
+++ test/command/parse-empty-command.test.ts
@@ -5,6 +5,13 @@ test("コマンド未入力状態をパースできる", () => {
   expect(parseEmptyCommand(data)).toEqual(data);
 });
 
+test("文字からJSONパースしたオブジェクトでも、正しくパースできる", () => {
+  const origin: EmptyCommand = { type: "EMPTY_COMMAND" };
+  const str = JSON.stringify(origin);
+  const data = JSON.parse(str);
+  expect(parseEmptyCommand(data)).toEqual(origin);
+});
+
 test("余計なプロパティを削除してパースする", () => {
   const origin: EmptyCommand = { type: "EMPTY_COMMAND" };
   const data = { ...origin, test: 12 };
