7692ed6a316797d746311de9079ee29e33e3e337
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-17 06:31:22 +0000

    PlayerStateをzod化した

--- src/index.ts
+++ src/index.ts
@@ -202,5 +202,5 @@ export {
   PilotStateSchema,
   PilotStateX,
 } from "./state/pilot-state";
-export { PlayerState, PlayerStateX } from "./state/player-state";
+export { parsePlayerState, PlayerState, PlayerStateSchema,PlayerStateX } from "./state/player-state";
 export { isPlayerDeath } from "./state/player-state/is-player-death";
