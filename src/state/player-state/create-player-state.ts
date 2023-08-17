65ac14bd4dd456652b62653bd54200c8b4d07f5e
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-17 06:16:50 +0000

    createPlayerStateを別ファイルに切り出した

---
+++ src/state/player-state/create-player-state.ts
@@ -0,0 +1,17 @@
+import { Player } from "../../player/player";
+import { createArmdozerState } from "../armdozer-state/create-armdozer-state";
+import { createPilotState } from "../pilot-state/create-pilot-state";
+import { PlayerState } from "./player-state";
+
+/**
+ * プレイヤーステートを生成する
+ * @param player プレイヤー情報
+ * @return 生成結果
+ */
+export function createPlayerState(player: Player): PlayerState {
+  return {
+    playerId: player.playerId,
+    armdozer: createArmdozerState(player.armdozer),
+    pilot: createPilotState(player.pilot),
+  };
+}
--- src/state/player-state/player-state.ts
+++ src/state/player-state/player-state.ts
@@ -21,19 +21,6 @@ export type PlayerStateX<BURST, PILOT> = Readonly<{
 export type PlayerState = PlayerStateX<Burst, PilotSkill>;
 
 /**
- * プレイヤーステートを生成する
- * @param player プレイヤー情報
- * @return 生成結果
- */
-export function createPlayerState(player: Player): PlayerState {
-  return {
-    playerId: player.playerId,
-    armdozer: createArmdozerState(player.armdozer),
-    pilot: createPilotState(player.pilot),
-  };
-}
-
-/**
  * プレイヤーの死亡判定を行う
  * @param defender 防御側のステータス
  * @return 判定結果、trueで死亡
