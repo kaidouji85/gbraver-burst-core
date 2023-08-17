0193be3d634c9b0b4aa7ad152a438e9c5f63459c
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-17 10:40:55 +0000

    parsePilotIdも同様の対応

--- test/player/parse-pilot-id.test.ts
+++ test/player/parse-pilot-id.test.ts
@@ -5,6 +5,13 @@ test("PilotIdはパースできる", () => {
   expect(parsePilotId(data)).toBe(data);
 });
 
+test("文字からJSONパースしたオブジェクトでも、正しくパースできる", () => {
+  const origin: PilotId = PilotIds.SHINYA;
+  const str = JSON.stringify(origin);
+  const data = JSON.parse(str);
+  expect(parsePilotId(data)).toBe(origin);
+});
+
 test("PilotId以外はnullを返す", () => {
   const data = 1000;
   expect(parsePilotId(data)).toBe(null);
