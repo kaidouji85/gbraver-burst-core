cf7e3e948dbe1c2ec224482053f2fee36be5fc6a
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-23 10:45:32 +0000

    SelectableSchemaのユニットテスト追加

---
+++ test/effect/input-command/selectable-schema.test.ts
@@ -0,0 +1,45 @@
+import { Selectable, SelectableSchema } from "../../../src";
+
+/** 有効なSelectable */
+const selectable: Selectable = {
+  playerId: "player1",
+  selectable: true,
+  command: [
+    {
+      type: "BATTERY_COMMAND",
+      battery: 0,
+    },
+    {
+      type: "BATTERY_COMMAND",
+      battery: 1,
+    },
+  ]
+};
+
+test("Selectableはパースできる", () => {
+  expect(SelectableSchema.parse(selectable)).toEqual(selectable);
+});
+
+test("文字からJSONパースしたオブジェクトでも、正しくパースできる", () => {
+  const str = JSON.stringify(selectable);
+  const data = JSON.parse(str);
+  expect(SelectableSchema.parse(data)).toEqual(selectable);
+});
+
+test("Selectable以外はパースできない", () => {
+  const data = {
+    player: "player1",
+    selectable: "true",
+    command: [
+      {
+        type: "BATTERY",
+        battery: 0,
+      },
+      {
+        type: "BATTERY",
+        battery: 1,
+      },
+    ]
+  };
+  expect(() => SelectableSchema.parse(data)).toThrow();
+});
