c44b5b3c49e7d0ea05538dda91bf266859e1bc8a
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-23 04:41:48 +0000

    EvenMatchをzod化

--- src/game/end-judging/game-end-judging.ts
+++ src/game/end-judging/game-end-judging.ts
@@ -30,6 +30,21 @@ export type EvenMatch = Readonly<{
   type: "EvenMatch";
 }>;
 
+/** EvenMatch zodスキーマ */
+export const EvenMatchSchema = z.object({
+  type: z.literal("EvenMatch"),
+});
+
+/**
+ * 任意オブジェクトをEvenMatchにパースする
+ * @param origin パース元
+ * @return パース結果、パースできない場合はnull
+ */
+export const parseEvenMatch = (origin: unknown): EvenMatch | null => {
+  const result = EvenMatchSchema.safeParse(origin);
+  return result.success ? result.data : null;
+}
+
 /** ゲーム続行 */
 export type GameContinue = Readonly<{
   type: "GameContinue";
