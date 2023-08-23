568c624e25363e530e1789c1871de5ad4f6b24a9
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-23 04:56:20 +0000

    parseGameEndJudgingのユニットテストを追加

---
+++ test/game/end-judging/parse-game-end-judging.test.ts
@@ -0,0 +1,36 @@
+import { GameEndJudging, parseGameEndJudging } from "../../../src";
+
+/** 有効なGameEndJudging */
+const gameEndJudges: GameEndJudging[] = [
+  {
+    type: "GameOver",
+    winner: "winner-player",
+  },
+  {
+    type: "EvenMatch",
+  },
+  {
+    type: "GameContinue",
+  }
+];
+
+test("GameEndJudgingはパースできる", () => {
+  gameEndJudges.forEach((gameEndJudging) => {
+    expect(parseGameEndJudging(gameEndJudging)).toEqual(gameEndJudging);
+  });
+});
+
+test("文字からJSONパースしたオブジェクトでも、正しくパースできる", () => {
+  gameEndJudges.forEach((gameEndJudging) => {
+    const str = JSON.stringify(gameEndJudging);
+    const data = JSON.parse(str);
+    expect(parseGameEndJudging(data)).toEqual(gameEndJudging);
+  });
+});
+
+test("GameEndJudging以外はパースできない", () => {
+  const data = {
+    name: "GameEndJudging",
+  };
+  expect(parseGameEndJudging(data)).toBeNull();
+});
