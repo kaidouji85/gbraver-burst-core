7c0a3470994bd60cddf4cda46fe9ba16dd0f014f
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-23 04:33:25 +0000

    GameOverをzod化

--- src/game/end-judging/game-end-judging.ts
+++ src/game/end-judging/game-end-judging.ts
@@ -1,4 +1,6 @@
-import type { PlayerId } from "../../player/player";
+import { z } from "zod";
+
+import { PlayerId, PlayerIdSchema } from "../../player/player";
 
 /** 勝負あり */
 export type GameOver = Readonly<{
@@ -6,6 +8,22 @@ export type GameOver = Readonly<{
   /** 勝利したプレイヤーのID */
   winner: PlayerId;
 }>;
+
+/** GameOver zodスキーマ */
+export const GameOverSchema = z.object({
+  type: z.literal("GameOver"),
+  winner: PlayerIdSchema,
+});
+
+/**
+ * 任意オブジェクトをGameOverにパースする
+ * @param origin パース元
+ * @return パース結果、パースできない場合はnull
+ */
+export const parseGameOver = (origin: unknown): GameOver | null => {
+  const result = GameOverSchema.safeParse(origin);
+  return result.success ? result.data : null;
+}
 
 /** 引き分け */
 export type EvenMatch = Readonly<{
