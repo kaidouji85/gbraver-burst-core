a3149f5e159493bf6db1defeded152d235b4d1c9
Y.Takeuchi <kaidouji85@gmail.com>
2023-08-28 09:12:34 +0000

    EndArmdozerEffectSchemaのユニットテスト追加

---
+++ test/effect/update-remaining-turn/end-armdozer-effect-schema.test.ts
@@ -0,0 +1,31 @@
+import { EndArmdozerEffect, EndArmdozerEffectSchema } from "../../../src";
+
+/** 有効なEndArmdozerEffect */
+const endArmdozerEffect: EndArmdozerEffect = {
+  playerId: "player1",
+  effect: {
+    type: "CorrectPower",
+    power: 1000,
+    period: {
+      type: "TurnLimit",
+      remainingTurn: 2,
+    },
+  }
+};
+
+test("EndArmdozerEffectはパースできる", () => {
+  expect(EndArmdozerEffectSchema.parse(endArmdozerEffect)).toEqual(endArmdozerEffect);
+});
+
+test("文字からJSONパースしたオブジェクトでも、正しくパースできる", () => {
+  const str = JSON.stringify(endArmdozerEffect);
+  const data = JSON.parse(str);
+  expect(EndArmdozerEffectSchema.parse(data)).toEqual(endArmdozerEffect);
+});
+
+test("EndArmdozerEffect以外はパースできない", () => {
+  const data = {
+    type: "EndArmdozerEffect",
+  };
+  expect(() => EndArmdozerEffectSchema.parse(data)).toThrow();
+});
