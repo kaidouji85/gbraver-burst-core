67ae6a245f7b7dd648fd0a78e9442c1f1e565bb9
Y.Takeuchi <kaidouji85@gmail.com>
2023-10-06 03:03:06 +0000

    UpdateRemainingTurnをtoMatchSnapshotで置き換えた

--- test/effect/update-remaining-turn/update-remaining-turn.test.ts
+++ test/effect/update-remaining-turn/update-remaining-turn.test.ts
@@ -1,18 +1,7 @@
-import path from "path";
-
 import { updateRemainingTurn } from "../../../src/effect/update-remaning-turn";
-import {
-  exportSnapShotJSON,
-  importSnapShotJSON,
-  shouldUpdateSnapShot,
-} from "../../snap-shot";
-import * as Data from "./update-remaining-turn.data";
+import { lastState } from "./update-remaining-turn.data";
 
 test("効果継続ターン更新が正しく処理される", () => {
-  const { lastState } = Data;
   const result = updateRemainingTurn(lastState);
-  const snapShotPath = path.join(__dirname, "update-remaining-turn.json");
-  shouldUpdateSnapShot() && exportSnapShotJSON(snapShotPath, result);
-  const snapShot = importSnapShotJSON(snapShotPath);
-  expect(result).toEqual(snapShot);
+  expect(result).toMatchSnapshot();
 });
