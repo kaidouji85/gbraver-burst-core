7e453f55777c5c520209b9888482d73c5b461cb1
Y.Takeuchi <kaidouji85@gmail.com>
2024-02-14 02:05:03 +0000

    不要な関数を削除した

--- src/effect/burst/index.ts
+++ src/effect/burst/index.ts
@@ -2,90 +2,9 @@ import { Burst } from "../../player/burst";
 import { RecoverBattery } from "../../player/burst/recover-battery";
 import { PlayerId } from "../../player/player";
 import { GameState, GameStateX } from "../../state/game-state";
-import { PlayerState } from "../../state/player-state";
-import { batteryLimitBreak } from "./battery-limit-break";
-import { buffPower } from "./buff-power";
 import { BurstEffect } from "./burst-effect";
 import { BurstInvoke, BurstInvokeResult } from "./burst-invoke";
-import { continuousActivePlayer } from "./continuous-active-player";
-import { lightningBarrier } from "./lightning-barrier";
-import { deprecatedRecoverBattery, recoverBattery } from "./recover-battery";
-
-/**
- * @deprecated
- * バースト効果を発動する
- * @param lastState 最新状態
- * @param burstPlayerId バーストするプレイヤーID
- * @return 更新結果、更新できない場合は例外を投げる
- */
-function deprecatedInvokeBurst(
-  lastState: GameState,
-  burstPlayerId: PlayerId,
-): GameStateX<BurstEffect> {
-  const burstPlayer = lastState.players.find(
-    (v) => v.playerId === burstPlayerId,
-  );
-  if (!burstPlayer) {
-    throw new Error("not found burst player");
-  }
-
-  if (burstPlayer.armdozer.burst.type === "RecoverBattery") {
-    return deprecatedRecoverBattery(
-      lastState,
-      burstPlayerId,
-      burstPlayer.armdozer.burst,
-    );
-  }
-
-  if (burstPlayer.armdozer.burst.type === "BuffPower") {
-    return buffPower(lastState, burstPlayerId, burstPlayer.armdozer.burst);
-  }
-
-  if (burstPlayer.armdozer.burst.type === "LightningBarrier") {
-    return lightningBarrier(
-      lastState,
-      burstPlayerId,
-      burstPlayer.armdozer.burst,
-    );
-  }
-
-  if (burstPlayer.armdozer.burst.type === "ContinuousAttack") {
-    return continuousActivePlayer(
-      lastState,
-      burstPlayerId,
-      burstPlayer.armdozer.burst,
-    );
-  }
-
-  if (burstPlayer.armdozer.burst.type === "BatteryLimitBreak") {
-    return batteryLimitBreak(
-      lastState,
-      burstPlayerId,
-      burstPlayer.armdozer.burst,
-    );
-  }
-
-  throw new Error("burst not found");
-}
-
-/**
- * @deprecated
- * 指定したプレイヤーのバーストを利用不能にする
- * @param lastState 最新状態
- * @return 更新結果
- */
-export function disableBurst(
-  lastState: GameStateX<BurstEffect>,
-): GameStateX<BurstEffect> {
-  const updateBurstPlayer = (burstPlayer: PlayerState) => ({
-    ...burstPlayer,
-    armdozer: { ...burstPlayer.armdozer, enableBurst: false },
-  });
-  const players = lastState.players.map((v) =>
-    v.playerId === lastState.effect.burstPlayer ? updateBurstPlayer(v) : v,
-  );
-  return { ...lastState, players };
-}
+import { recoverBattery } from "./recover-battery";
 
 /**
  * バースト種別に応じた効果を適用する
