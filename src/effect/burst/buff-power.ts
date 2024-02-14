b0cdb23bf2d01e490f76b859f55abbcbccf5426f
Y.Takeuchi <kaidouji85@gmail.com>
2024-02-14 02:26:12 +0000

    buffPower改善

--- src/effect/burst/buff-power.ts
+++ src/effect/burst/buff-power.ts
@@ -1,6 +1,32 @@
 import { BuffPower } from "../../player/burst/buff-power";
+import { PlayerState } from "../../state/player-state";
 import { BurstInvoke, BurstInvokeResult } from "./burst-invoke";
 import { burstRecoverBattery } from "./burst-recover-battery";
+
+/**
+ * バースト発動者のステートを更新する
+ * @param invoker バースト発動者のステート
+ * @param burst バースト情報
+ * @return バースト発動後のステート
+ */
+const updateInvoker = (invoker: PlayerState, burst: BuffPower): PlayerState => ({
+  ...invoker,
+  armdozer: {
+    ...invoker.armdozer,
+    battery: burstRecoverBattery(invoker.armdozer, burst),
+    effects: [
+      ...invoker.armdozer.effects,
+      {
+        type: "CorrectPower",
+        power: burst.buffPower,
+        period: {
+          type: "TurnLimit",
+          remainingTurn: burst.duration,
+        },
+      },
+    ],
+  },
+});
 
 /**
  * バースト 攻撃アップ 発動
@@ -10,24 +36,7 @@ import { burstRecoverBattery } from "./burst-recover-battery";
 export function buffPower(params: BurstInvoke<BuffPower>): BurstInvokeResult {
   const { invoker, other, burst } = params;
   return {
-    invoker: {
-      ...invoker,
-      armdozer: {
-        ...invoker.armdozer,
-        battery: burstRecoverBattery(invoker.armdozer, burst),
-        effects: [
-          ...invoker.armdozer.effects,
-          {
-            type: "CorrectPower",
-            power: burst.buffPower,
-            period: {
-              type: "TurnLimit",
-              remainingTurn: burst.duration,
-            },
-          },
-        ],
-      },
-    },
+    invoker: updateInvoker(invoker, burst),
     other,
   };
 }
