9feb807160fcf35b74a554dcdafceebda9879b93
Y.Takeuchi <kaidouji85@gmail.com>
2024-02-15 10:41:49 +0000

    JSDocをより充実させた

--- src/effect/burst/burst-invoke-result.ts
+++ src/effect/burst/burst-invoke-result.ts
@@ -2,8 +2,8 @@ import { PlayerState } from "../../state/player-state";
 
 /** バースト発動結果 */
 export type BurstInvokeResult = {
-  /** バースト発動プレイヤー バースト発動後 */
+  /** バースト発動プレイヤー バースト発動後のステート */
   invoker: PlayerState;
-  /** それ以外のプレイヤー */
+  /** それ以外のプレイヤー バースト発動後のステート */
   other: PlayerState;
 };
