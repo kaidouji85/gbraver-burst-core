62a4171fbbdcb40b4d0197250402cea5247c2277
Y.Takeuchi <kaidouji85@gmail.com>
2024-02-23 12:59:37 +0900

    JSDocに注意書きを追加した

--- src/game/game-flow.ts
+++ src/game/game-flow.ts
@@ -10,6 +10,22 @@ type AddStateHistory = (state: GameState) => GameState[];
 
 /**
  * ゲームフローを開始する
+ * 本関数は以下の呼ばれ方を想定している
+ *
+ * (1)ゲーム開始のように最新ステートがない場合
+ * startGameFlow([
+ *   () => [startGame(players)],      // 最初の関数には最新ステートがないため引数なし
+ *   (state) => [anyEffect(state)],   // ２個目以降からstateを引数によってよい
+ *   // ...
+ * ]);                                // 最新ステートがないので引数lastStateは指定しない
+ *
+ * (2)それ以外の場合
+ * startGameFlow([
+ *   (state) => [anyEffectX(state)],  // 最初の関数には最新ステートがあるため引数あり
+ *   (state) => [anyEffectY(state)],
+ *   // ...
+ * ], lastState);                     // かならず最新ステートを引数に渡す
+ *
  * @param fns ステートヒストリー追加関数を順に適用する
  * @param lastState フロー開始前の最新ステート、指定されない場合はEMPTY_GAME_STATEがセットされる
  * @return 本フローで追加されたゲームステート
