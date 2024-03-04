eef246d40476a89f39ff59a50beab688fc66038a
Y.Takeuchi <kaidouji85@gmail.com>
2024-03-04 04:12:41 +0000

    もう一つのテストケースもtest.each対応した

--- test/effect/start-game/first-turn-player.test.ts
+++ test/effect/start-game/first-turn-player.test.ts
@@ -22,27 +22,32 @@ test.each([
   [fastPlayer, slowPlayer, 0.5],
   [slowPlayer, fastPlayer, 0],
   [slowPlayer, fastPlayer, 0.5],
-])('ランダム値、引数順番に関わらず、機動力が高いプレイヤーが先行となる', (p1, p2, random) => {
-  expect(getFirstTurnPlayer(p1, p2, random)).toBe(fastPlayer.playerId);
-});
+])(
+  "ランダム値、引数順番に関わらず、機動力が高いプレイヤーが先行となる",
+  (p1, p2, random) => {
+    expect(getFirstTurnPlayer(p1, p2, random)).toBe(fastPlayer.playerId);
+  },
+);
 
-test("スピードが同じ場合にはランダムで先行を決定", () => {
-  const player1: PlayerState = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: "player1",
-    armdozer: { ...EMPTY_ARMDOZER_STATE, speed: 1000 },
-  };
-  const player2: PlayerState = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: "player2",
-    armdozer: { ...EMPTY_ARMDOZER_STATE, speed: 1000 },
-  };
+/** 機動力が同じプレイヤー1 */
+const sameSpeedPlayer1: PlayerState = {
+  ...EMPTY_PLAYER_STATE,
+  playerId: "player1",
+  armdozer: { ...EMPTY_ARMDOZER_STATE, speed: 1000 },
+};
 
-  const data = [
-    [0, player1.playerId],
-    [0.5, player2.playerId],
-  ] as const;
-  data.forEach(([random, expected]) => {
-    expect(getFirstTurnPlayer(player1, player2, random)).toBe(expected);
-  });
+/** 機動力が同じプレイヤー2 */
+const sameSpeedPlayer2: PlayerState = {
+  ...EMPTY_PLAYER_STATE,
+  playerId: "player2",
+  armdozer: { ...EMPTY_ARMDOZER_STATE, speed: 1000 },
+};
+
+test.each([
+  [0, sameSpeedPlayer1.playerId],
+  [0.5, sameSpeedPlayer2.playerId],
+])("スピードが同じ場合にはランダムで先行を決定", (random, expected) => {
+  expect(getFirstTurnPlayer(sameSpeedPlayer1, sameSpeedPlayer2, random)).toBe(
+    expected,
+  );
 });
