95a4bfcb722c406b328490160366edc7b8b29864
Y.Takeuchi <kaidouji85@gmail.com>
2024-03-06 09:30:00 +0000

    inputCommandのユニットテストを改善

--- test/effect/input-command/input-command.test.ts
+++ test/effect/input-command/input-command.test.ts
@@ -1,77 +1,78 @@
+import { BatteryCommand, BurstCommand, PlayerId } from "../../../src";
 import { inputCommand } from "../../../src/effect/input-command";
 import { EMPTY_ARMDOZER_STATE } from "../../../src/empty/armdozer";
 import { EMPTY_GAME_STATE } from "../../../src/empty/game-state";
 import { EMPTY_PLAYER_STATE } from "../../../src/empty/player";
 
+/**
+ * プレイヤーを生成する
+ * @param playerId プレイヤーID
+ * @param battery バッテリー
+ * @param enableBurst バースト発動可能か否か、trueで発動可能
+ * @return 生成されたプレイヤー
+ */
+const createPlayer = (
+  playerId: PlayerId,
+  battery: number,
+  enableBurst: boolean,
+) => ({
+  ...EMPTY_PLAYER_STATE,
+  playerId,
+  armdozer: {
+    ...EMPTY_ARMDOZER_STATE,
+    battery,
+    maxBattery: 5,
+    enableBurst,
+  },
+});
+
+/**
+ * バッテリーコマンドを生成する
+ * @param battery バッテリー
+ * @return 生成されたバッテリーコマンド
+ */
+const createBatteryCommand = (battery: number): BatteryCommand => ({
+  type: "BATTERY_COMMAND",
+  battery,
+});
+
+/** バーストコマンド */
+const burstCommand: BurstCommand = {
+  type: "BURST_COMMAND",
+};
+
 test("戦闘後のコマンド入力フェイズが正しく適用される", () => {
-  const player01 = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: "player01",
-    armdozer: {
-      ...EMPTY_ARMDOZER_STATE,
-      battery: 5,
-      maxBattery: 5,
-      enableBurst: true,
-    },
-  };
-  const player02 = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: "player02",
-    armdozer: {
-      ...EMPTY_ARMDOZER_STATE,
-      battery: 3,
-      maxBattery: 5,
-      enableBurst: false,
-    },
-  };
-  const result = inputCommand(
-    { ...EMPTY_GAME_STATE, players: [player01, player02] },
-    player01.playerId,
-    {
-      type: "BATTERY_COMMAND",
-      battery: 3,
-    },
-    player02.playerId,
-    {
-      type: "BATTERY_COMMAND",
-      battery: 3,
-    },
-  );
-  expect(result).toMatchSnapshot("after-battle");
+  const player01 = createPlayer("player01", 5, true);
+  const player01Command = createBatteryCommand(3);
+  const player02 = createPlayer("player02", 3, false);
+  const player02Command = createBatteryCommand(3);
+  const lastState = { ...EMPTY_GAME_STATE, players: [player01, player02] };
+
+  expect(
+    inputCommand(
+      lastState,
+      player01.playerId,
+      player01Command,
+      player02.playerId,
+      player02Command,
+    ),
+  ).toMatchSnapshot("after-battle");
 });
 
 test("効果適用フロー後のコマンド入力フェイズ効果が正しく処理される", () => {
-  const player01 = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: "player01",
-    armdozer: {
-      ...EMPTY_ARMDOZER_STATE,
-      battery: 2,
-      maxBattery: 5,
-      enableBurst: true,
-    },
-  };
-  const player02 = {
-    ...EMPTY_PLAYER_STATE,
-    playerId: "player02",
-    armdozer: {
-      ...EMPTY_ARMDOZER_STATE,
-      battery: 3,
-      maxBattery: 5,
-      enableBurst: false,
-    },
-  };
-  const result = inputCommand(
-    { ...EMPTY_GAME_STATE, players: [player01, player02] },
-    player01.playerId,
-    {
-      type: "BATTERY_COMMAND",
-      battery: 3,
-    },
-    player02.playerId,
-    {
-      type: "BURST_COMMAND",
-    },
-  );
-  expect(result).toMatchSnapshot("after-effect-activation");
+  const player01 = createPlayer("player01", 2, true);
+  const player01Command = createBatteryCommand(3);
+  const player02 = createPlayer("player02", 3, false);
+  const player02Command = burstCommand;
+  const lastState = { ...EMPTY_GAME_STATE, players: [player01, player02] };
+
+  expect(
+    inputCommand(
+      lastState,
+      player01.playerId,
+      player01Command,
+      player02.playerId,
+      player02Command,
+    ),
+  ).toMatchSnapshot("after-effect-activation");
 });
