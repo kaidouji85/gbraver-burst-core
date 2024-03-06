01f6e7a2fed734959ff048ae9405f1270a797916
Y.Takeuchi <kaidouji85@gmail.com>
2024-03-06 10:24:44 +0000

    ユニットテストでQuickCommandを使わないようにした

--- test/effect/input-command/is-no-choice.test.ts
+++ test/effect/input-command/is-no-choice.test.ts
@@ -1,38 +1,51 @@
-import type { BatteryCommand, Command, QuickCommand } from "../../../src";
+import type {
+  BatteryCommand,
+  BurstCommand,
+  PilotSkillCommand,
+} from "../../../src";
 import { isNoChoice } from "../../../src/effect/input-command";
 
-const BATTERY_COMMAND: BatteryCommand = {
+/** バッテリーコマンド */
+const battery: BatteryCommand = {
   type: "BATTERY_COMMAND",
   battery: 1,
 };
-const QUICK_COMMAND: QuickCommand = {
+
+/** バーストコマンド */
+const burst: BurstCommand = {
   type: "BURST_COMMAND",
 };
 
-test("相手だけがクイックコマンドを使った場合、コマンド選択不可能となる", () => {
-  const myCommand: Command = BATTERY_COMMAND;
-  const otherCommand: Command = QUICK_COMMAND;
-  const result = isNoChoice(myCommand, otherCommand);
-  expect(result).toBe(true);
-});
+/** パイロットスキルコマンド */
+const pilotSkill: PilotSkillCommand = {
+  type: "PILOT_SKILL_COMMAND",
+};
 
-test("自分だけがクイックコマンドを使った場合、コマンド選択可能である", () => {
-  const myCommand = QUICK_COMMAND;
-  const otherCommand: Command = BATTERY_COMMAND;
-  const result = isNoChoice(myCommand, otherCommand);
-  expect(result).toBe(false);
-});
-
-test("違いにバッテリーコマンドの場合、操作可能である", () => {
-  const myCommand = BATTERY_COMMAND;
-  const otherCommand: Command = BATTERY_COMMAND;
-  const result = isNoChoice(myCommand, otherCommand);
-  expect(result).toBe(false);
-});
-
-test("違いにクイックコマンドの場合、操作可能である", () => {
-  const myCommand = QUICK_COMMAND;
-  const otherCommand = QUICK_COMMAND;
-  const result = isNoChoice(myCommand, otherCommand);
-  expect(result).toBe(false);
-});
+test.each([
+  { myCommand: battery, otherCommand: battery, expected: false },
+  { myCommand: battery, otherCommand: burst, expected: true },
+  {
+    myCommand: battery,
+    otherCommand: pilotSkill,
+    expected: true,
+  },
+  { myCommand: burst, otherCommand: battery, expected: false },
+  { myCommand: burst, otherCommand: burst, expected: false },
+  { myCommand: burst, otherCommand: pilotSkill, expected: false },
+  {
+    myCommand: pilotSkill,
+    otherCommand: battery,
+    expected: false,
+  },
+  { myCommand: pilotSkill, otherCommand: burst, expected: false },
+  {
+    myCommand: pilotSkill,
+    otherCommand: pilotSkill,
+    expected: false,
+  },
+])(
+  "自分コマンド=バッテリー and 相手コマンド=(バースト or パイロットスキル) の場合、自分はコマンド選択不可となる[myCommand=$myCommand.type, otherCommand=$otherCommand.type]",
+  ({ myCommand, otherCommand, expected }) => {
+    expect(isNoChoice(myCommand, otherCommand)).toBe(expected);
+  },
+);
