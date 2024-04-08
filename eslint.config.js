968013332f4d8f1738ced5a95e40017da5f06626
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-08 09:20:58 +0000

    eslint-plugin-simple-import-sortを追加

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import jest from 'eslint-plugin-jest';
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
   {
     files: ['test/**'],
     ...jest.configs['flat/recommended'],
