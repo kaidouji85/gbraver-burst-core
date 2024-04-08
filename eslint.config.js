5ef705c22c93c556ac9373cf2ddfcdc8e1d6a71b
Y.Takeuchi <kaidouji85@gmail.com>
2024-04-08 09:17:45 +0000

    jestのルールを追加した

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,15 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import jest from 'eslint-plugin-jest';
 
 export default [
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files: ['test/**'],
+    ...jest.configs['flat/recommended'],
+    rules: {
+      ...jest.configs['flat/recommended'].rules,
+    },
+  },
 ];
