bf95ac8d9acb18c64b4431b270aa88c5f8d99e40
kaidouji85 <kaidouji85@gmail.com>
2024-05-09 02:47:55 +0000

    removeTurnStartBatteryCorrectのユニットテストを追加した

---
+++ test/effect/remove-turn-start-battery-correct.test.ts
@@ -0,0 +1,31 @@
+import { CorrectPower, TurnStartBatteryCorrect } from "../../src";
+import { removeTurnStartBatteryCorrect } from "../../src/effect/remove-turn-start-battery-correct";
+
+/** ターン開始時バッテリー回復量の補正  */
+const turnStartBatteryCorrect: TurnStartBatteryCorrect = {
+  type: "TurnStartBatteryCorrect",
+  correctBattery: 1,
+  period: {
+    type: "SpecialPeriod",
+  },
+};
+
+/** TurnStartBatteryCorrect以外 */
+const other: CorrectPower = {
+  type: "CorrectPower",
+  power: 1000,
+  period: {
+    type: "TurnLimit",
+    remainingTurn: 1,
+  },
+};
+
+test("TurnStartBatteryCorrectだけを取り除く", () => {
+  const data = [other, other, turnStartBatteryCorrect, other];
+  expect(removeTurnStartBatteryCorrect(data)).toEqual([other, other, other]);
+});
+
+test("TurnStartBatteryCorrectがなければそのまま", () => {
+  const data = [other, other, other];
+  expect(removeTurnStartBatteryCorrect(data)).toEqual([other, other, other]);
+});
