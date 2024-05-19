fb9ab9dde3e0c47bb347c80f6ac21e16d8daa29b
Y.Takeuchi <kaidouji85@gmail.com>
2024-05-20 00:44:49 +0900

    battleをリファクタリングした

--- src/effect/battle/players/update-defender.ts
+++ src/effect/battle/players/update-defender.ts
@@ -1,41 +1,32 @@
-import type { PlayerState } from "../../../state/player-state";
-import type { BattleResult } from "../result/battle-result";
-
-/**
- * 戦闘結果に応じて防御側ステータスを更新する
- *
- * @param result 戦闘結果
- * @param defender 更新前の防御側ステータス
- * @returns 更新された防御側ステータス
- */
-export function updateDefender(
-  result: BattleResult,
-  defender: PlayerState,
-): PlayerState {
-  return {
-    ...defender,
-    armdozer: {
-      ...defender.armdozer,
-      hp: updateDefenderHP(result, defender.armdozer.hp),
-    },
-  };
-}
+import { PlayerState } from "../../../state/player-state";
+import { BattleResult } from "../result/battle-result";
 
 /**
  * 戦闘結果に応じて、防御側のHPを更新する
- *
  * @param result 戦闘結果
  * @param hp 更新前の防御側HP
  * @returns 更新後の防御側HP
  */
-export function updateDefenderHP(result: BattleResult, hp: number): number {
-  switch (result.name) {
-    case "NormalHit":
-    case "Guard":
-    case "CriticalHit":
-      return hp - result.damage;
+const updateDefenderHP = (result: BattleResult, hp: number) =>
+  result.name === "NormalHit" ||
+  result.name === "Guard" ||
+  result.name === "CriticalHit"
+    ? hp - result.damage
+    : hp;
 
-    default:
-      return hp;
-  }
-}
+/**
+ * 戦闘結果に応じて防御側ステータスを更新する
+ * @param result 戦闘結果
+ * @param defender 更新前の防御側ステータス
+ * @returns 更新された防御側ステータス
+ */
+export const updateDefender = (
+  result: BattleResult,
+  defender: PlayerState,
+): PlayerState => ({
+  ...defender,
+  armdozer: {
+    ...defender.armdozer,
+    hp: updateDefenderHP(result, defender.armdozer.hp),
+  },
+});
