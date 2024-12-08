340b5424f6038b94d47cb4806985e919ee94225a
Y.Takeuchi <kaidouji85@gmail.com>
2024-12-08 16:08:13 +0900

    プレイヤーを生成できるようにした

--- test/game/progress/effect-activation-flow.test.ts
+++ test/game/progress/effect-activation-flow.test.ts
@@ -1,41 +1,69 @@
-import type {
+import {
   BatteryCommand,
+  Burst,
   BurstCommand,
   GameState,
   PlayerCommand,
+  PlayerId,
   PlayerState,
 } from "../../../src";
-import type { PilotSkillCommand } from "../../../src/command/pilot-skill";
+import { PilotSkillCommand } from "../../../src/command/pilot-skill";
 import { EMPTY_GAME_STATE } from "../../../src/empty/game-state";
 import { EMPTY_PLAYER_STATE } from "../../../src/empty/player";
 import { effectActivationFlow } from "../../../src/game/progress/effect-activation-flow";
 
-const ATTACKER: PlayerState = { ...EMPTY_PLAYER_STATE, playerId: "attacker" };
-const DEFENDER: PlayerState = { ...EMPTY_PLAYER_STATE, playerId: "defender" };
+/**
+ * テストプレイヤーを生成うる
+ * @param playerId プレイヤーID
+ * @param burst バースト
+ */
+const createPlayer = (
+  playerId: PlayerId,
+  burst: Burst = {
+    type: "RecoverBattery",
+    recoverBattery: 5,
+    turnStartBatteryCorrect: 1,
+  },
+): PlayerState => ({
+  ...EMPTY_PLAYER_STATE,
+  playerId,
+  armdozer: {
+    ...EMPTY_PLAYER_STATE.armdozer,
+    burst,
+  },
+});
+
+/** バッテリーコマンド */
 const BATTERY_COMMAND: BatteryCommand = {
   type: "BATTERY_COMMAND",
   battery: 1,
 };
+
+/** バーストコマンド */
 const BURST_COMMAND: BurstCommand = {
   type: "BURST_COMMAND",
 };
+
+/** パイロットスキルコマンド */
 const PILOT_SKILL_COMMAND: PilotSkillCommand = {
   type: "PILOT_SKILL_COMMAND",
 };
 
 test("一人だけ効果適用する場合でも正しく処理される", () => {
+  const attacker = createPlayer("attacker");
+  const defender = createPlayer("defender");
   const state: GameState = {
     ...EMPTY_GAME_STATE,
-    players: [DEFENDER, ATTACKER],
-    activePlayerId: ATTACKER.playerId,
+    players: [defender, attacker],
+    activePlayerId: attacker.playerId,
   };
   const commands: [PlayerCommand, PlayerCommand] = [
     {
-      playerId: ATTACKER.playerId,
+      playerId: attacker.playerId,
       command: BURST_COMMAND,
     },
     {
-      playerId: DEFENDER.playerId,
+      playerId: defender.playerId,
       command: BATTERY_COMMAND,
     },
   ];
@@ -44,18 +72,20 @@ test("一人だけ効果適用する場合でも正しく処理される", () =>
 });
 
 test("二人とも効果適用する場合でも正しく処理される", () => {
+  const attacker = createPlayer("attacker");
+  const defender = createPlayer("defender");
   const state: GameState = {
     ...EMPTY_GAME_STATE,
-    players: [DEFENDER, ATTACKER],
-    activePlayerId: ATTACKER.playerId,
+    players: [defender, attacker],
+    activePlayerId: attacker.playerId,
   };
   const commands: [PlayerCommand, PlayerCommand] = [
     {
-      playerId: ATTACKER.playerId,
+      playerId: attacker.playerId,
       command: BURST_COMMAND,
     },
     {
-      playerId: DEFENDER.playerId,
+      playerId: defender.playerId,
       command: PILOT_SKILL_COMMAND,
     },
   ];
