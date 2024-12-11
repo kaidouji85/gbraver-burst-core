195688dd30d5273bdba19a8aa3e30bb398a11919
kaidouji85 <kaidouji85@gmail.com>
2024-12-11 06:20:25 +0000

    jest設定ファイルにテストコード用ts-configを指定するようにした

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,8 +1,12 @@
+import { createDefaultPreset } from "ts-jest";
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
+export default {
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: "coverage",
   testPathIgnorePatterns: ["<rootDir>/lib/"],
+  ...createDefaultPreset({
+    tsconfig: "<rootDir>/tsconfig.test.json",
+  }),
 };
