6454bb6cc70d65bfeefedcde5eefeb0a584c7271
Y.Takeuchi <kaidouji85@gmail.com>
2025-01-12 14:56:45 +0900

    IneffectiveSchemaのユニットテストを追加

---
+++ test/player/burst/ineffective.test.ts
@@ -0,0 +1,25 @@
+import { Ineffective, IneffectiveSchema } from "../../../src";
+
+/** Ineffective */
+const ineffective: Ineffective = {
+  type: "Ineffective",
+  recoverBattery: 3,
+};
+
+test("Ineffectiveはパースできる", () => {
+  expect(IneffectiveSchema.parse(ineffective)).toEqual(ineffective);
+});
+
+test("文字からJSONパースしたオブジェクトでも、正しくパースできる", () => {
+  const str = JSON.stringify(ineffective);
+  const data = JSON.parse(str);
+  expect(IneffectiveSchema.parse(data)).toEqual(ineffective);
+});
+
+test("Ineffective 以外は例外を投げる", () => {
+  const data = {
+    type: "Innffective",
+    recover: 3,
+  };
+  expect(() => IneffectiveSchema.parse(data)).toThrow();
+});
