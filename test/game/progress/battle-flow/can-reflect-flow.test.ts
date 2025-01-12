009ee9cbc634fba6e2d1248b2d165244ef042635
Y.Takeuchi <kaidouji85@gmail.com>
2025-01-12 18:02:14 +0900

    ユニットテストをリファクタリングした

--- test/game/progress/battle-flow/can-reflect-flow.test.ts
+++ test/game/progress/battle-flow/can-reflect-flow.test.ts
@@ -1,37 +1,42 @@
-import type { BattleResult } from "../../../../src";
+import { CriticalHit, Feint, Guard, Miss, NormalHit } from "../../../../src";
 import { canReflectFlow } from "../../../../src/game/progress/battle-flow/reflect-flow";
 
+/** 通常ヒット */
+const normalHit: NormalHit = { name: "NormalHit", damage: 2000 };
+
+/** ガード */
+const guard: Guard = { name: "Guard", damage: 1000 };
+
+/** クリティカル */
+const criticalHit: CriticalHit = { name: "CriticalHit", damage: 9999 };
+
+/** ミス */
+const miss: Miss = { name: "Miss" };
+
+/** フェイント */
+const feint: Feint = { name: "Feint", isDefenderMoved: true };
+
 test("通常ヒットの場合はダメージ反射を行う", () => {
-  const battleResult: BattleResult = {
-    name: "NormalHit",
-    damage: 1000,
-  };
-  const result = canReflectFlow(battleResult);
+  const result = canReflectFlow(normalHit);
   expect(result).toBe(true);
 });
 
 test("ガードの場合はダメージ反射を行う", () => {
-  const battleResult: BattleResult = {
-    name: "Guard",
-    damage: 1000,
-  };
-  const result = canReflectFlow(battleResult);
+  const result = canReflectFlow(guard);
   expect(result).toBe(true);
 });
 
 test("クリティカルの場合はダメージ反射を行う", () => {
-  const battleResult: BattleResult = {
-    name: "CriticalHit",
-    damage: 1000,
-  };
-  const result = canReflectFlow(battleResult);
+  const result = canReflectFlow(criticalHit);
   expect(result).toBe(true);
 });
 
 test("ミスの場合はダメージ反射をしない", () => {
-  const battleResult: BattleResult = {
-    name: "Miss",
-  };
-  const result = canReflectFlow(battleResult);
+  const result = canReflectFlow(miss);
+  expect(result).toBe(false);
+});
+
+test("フェイントの場合はダメージ反射をしない", () => {
+  const result = canReflectFlow(feint);
   expect(result).toBe(false);
 });
