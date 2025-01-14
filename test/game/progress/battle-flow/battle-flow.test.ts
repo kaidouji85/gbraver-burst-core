371c2b7b269fee7e7c1931db800c2853699e57f3
kaidouji85 <kaidouji85@gmail.com>
2025-01-14 04:39:47 +0000

    continue-gameのテストデータをコード中で生成するようにした

--- test/game/progress/battle-flow/battle-flow.test.ts
+++ test/game/progress/battle-flow/battle-flow.test.ts
@@ -1,12 +1,12 @@
 import { BatteryCommand } from "../../../../src/command/battery";
 import { EMPTY_ARMDOZER_STATE } from "../../../../src/empty/armdozer";
+import { EMPTY_GAME_STATE } from "../../../../src/empty/game-state";
 import { EMPTY_PLAYER_STATE } from "../../../../src/empty/player";
 import { PlayerCommandX } from "../../../../src/game/command/player-command";
 import { battleFlow } from "../../../../src/game/progress/battle-flow";
 import { PlayerId } from "../../../../src/player/player";
 import { ArmdozerEffect } from "../../../../src/state/armdozer-effect";
 import { PlayerState } from "../../../../src/state/player-state";
-import * as ContinuesGameData from "./battle-flow__continue-game.data";
 import * as DeathData from "./battle-flow__death.data";
 import * as DrawData from "./battle-flow__draw.data";
 
@@ -21,11 +21,11 @@ type AttackerOptions = {
 };
 
 /**
- * 攻撃側プレイヤーを生成する
+ * プレイヤーを生成する
  * @param options 生成オプション
- * @returns 攻撃側プレイヤー
+ * @returns プレイヤー
  */
-const createAttakcer = (options: AttackerOptions): PlayerState => {
+const createPlayer = (options: AttackerOptions): PlayerState => {
   const { playerId, battery, effects } = options;
   return {
     ...EMPTY_PLAYER_STATE,
@@ -48,16 +48,36 @@ const createAttakcer = (options: AttackerOptions): PlayerState => {
  * @param battery バッテリー値
  * @returns バッテリーコマンド
  */
-const createBatteryCommand = (playerId: PlayerId, battery: number): PlayerCommandX<BatteryCommand> => ({
+const createBatteryCommand = (
+  playerId: PlayerId,
+  battery: number,
+): PlayerCommandX<BatteryCommand> => ({
   playerId,
   command: { type: "BATTERY_COMMAND", battery },
 });
 
-
 test("戦闘したが、相手を倒しきれなかったのでゲーム続行", () => {
-  const { lastState, commands } = ContinuesGameData;
-  const result = battleFlow(lastState, commands);
-  expect(result).toMatchSnapshot("continue-game");
+  const attacker = createPlayer({
+    playerId: "attacker",
+    battery: 4,
+    effects: [],
+  });
+  const defender = createPlayer({
+    playerId: "defender",
+    battery: 5,
+    effects: [],
+  });
+  const lastState = {
+    ...EMPTY_GAME_STATE,
+    activePlayerId: attacker.playerId,
+    players: [attacker, defender],
+  };
+  expect(
+    battleFlow(lastState, [
+      createBatteryCommand(attacker.playerId, 2),
+      createBatteryCommand(defender.playerId, 1),
+    ]),
+  ).toMatchSnapshot("continue-game");
 });
 
 test("攻撃で防御側のHPを0以下にした場合、ゲームが終了する", () => {
