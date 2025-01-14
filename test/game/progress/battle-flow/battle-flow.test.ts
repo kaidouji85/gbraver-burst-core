c29ff8e94ecf3e3b9eddbdedca84eab7c57ab096
kaidouji85 <kaidouji85@gmail.com>
2025-01-14 04:34:43 +0000

    各種ヘルパー関数を追加した

--- test/game/progress/battle-flow/battle-flow.test.ts
+++ test/game/progress/battle-flow/battle-flow.test.ts
@@ -1,7 +1,58 @@
+import { BatteryCommand } from "../../../../src/command/battery";
+import { EMPTY_ARMDOZER_STATE } from "../../../../src/empty/armdozer";
+import { EMPTY_PLAYER_STATE } from "../../../../src/empty/player";
+import { PlayerCommandX } from "../../../../src/game/command/player-command";
 import { battleFlow } from "../../../../src/game/progress/battle-flow";
+import { PlayerId } from "../../../../src/player/player";
+import { ArmdozerEffect } from "../../../../src/state/armdozer-effect";
+import { PlayerState } from "../../../../src/state/player-state";
 import * as ContinuesGameData from "./battle-flow__continue-game.data";
 import * as DeathData from "./battle-flow__death.data";
 import * as DrawData from "./battle-flow__draw.data";
+
+/** プレイヤー生成オプション */
+type AttackerOptions = {
+  /** プレイヤーID */
+  playerId: PlayerId;
+  /** バッテリー */
+  battery: number;
+  /** 適用中の効果 */
+  effects: ArmdozerEffect[];
+};
+
+/**
+ * 攻撃側プレイヤーを生成する
+ * @param options 生成オプション
+ * @returns 攻撃側プレイヤー
+ */
+const createAttakcer = (options: AttackerOptions): PlayerState => {
+  const { playerId, battery, effects } = options;
+  return {
+    ...EMPTY_PLAYER_STATE,
+    playerId,
+    armdozer: {
+      ...EMPTY_ARMDOZER_STATE,
+      hp: 3000,
+      maxHp: 3000,
+      power: 2000,
+      battery,
+      maxBattery: 5,
+      effects,
+    },
+  };
+};
+
+/**
+ * バッテリーコマンドを生成する
+ * @param playerId プレイヤーID
+ * @param battery バッテリー値
+ * @returns バッテリーコマンド
+ */
+const createBatteryCommand = (playerId: PlayerId, battery: number): PlayerCommandX<BatteryCommand> => ({
+  playerId,
+  command: { type: "BATTERY_COMMAND", battery },
+});
+
 
 test("戦闘したが、相手を倒しきれなかったのでゲーム続行", () => {
   const { lastState, commands } = ContinuesGameData;
