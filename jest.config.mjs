36f5ccd4910c0274c37945b7c129676fcf4f697f
kaidouji85 <kaidouji85@gmail.com>
2025-03-06 02:59:01 +0000

    prettierの範囲を広めた

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,7 +2,7 @@ import { createDefaultPreset } from "ts-jest";
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  testEnvironment: 'node',
+  testEnvironment: "node",
   collectCoverage: true,
   coverageDirectory: "coverage",
   testPathIgnorePatterns: ["<rootDir>/lib/"],
