d2aa5cd4bc1673f57f64d25851af8f7e29e44f48
Y.Takeuchi <kaidouji85@gmail.com>
2025-06-14 16:52:25 +0900

    feat: update import paths for game state and player state in burst effect test files

--- test/effect/burst/buff-power.test.ts
+++ test/effect/burst/buff-power.test.ts
@@ -1,12 +1,10 @@
-import {
-  EMPTY_ARMDOZER_EFFECT,
-  type GameState,
-  type PlayerState,
-} from "../../../src";
 import { burst } from "../../../src/effect/burst";
+import { EMPTY_ARMDOZER_EFFECT } from "../../../src/empty/amrdozer-effect";
 import { EMPTY_ARMDOZER_STATE } from "../../../src/empty/armdozer";
 import { EMPTY_GAME_STATE } from "../../../src/empty/game-state";
 import { EMPTY_PLAYER_STATE } from "../../../src/empty/player";
+import { GameState } from "../../../src/state/game-state";
+import { PlayerState } from "../../../src/state/player-state";
 
 /** バースト発動者 */
 const burstPlayer: PlayerState = {
